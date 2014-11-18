5a001993c2619ff1635230022017b54f07f12840
karthikraremile <karthik.srivatsa@raremile.com>
2014-11-18 18:20:03 +0530

    Made all the 12 tweaks!

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/generator/package-info.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/generator/package-info.java
@@ -2,8 +2,5 @@
  * This package contains the Interface and Classes for generating encrypted urls
  * and also decrypting them.
  */
-/**
- * @author Karthik Srivatsa
- *
- */
+//JIRA: SS-6: By RM03
 package com.realtech.socialsurvey.core.services.generator;
