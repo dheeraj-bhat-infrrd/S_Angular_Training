e6f3aa260c300091706f482a46c45e6d9d866ec8
agrawal-shubham <shubham@raremile.com>
2014-11-18 15:11:21 +0530

    Modified the Object model according to latest DB model

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/LicenseDetail.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/LicenseDetail.java
@@ -1,5 +1,4 @@
 package com.realtech.socialsurvey.core.entities;
-//JIRA: SS-1: By RM06: BOC
 
 import java.io.Serializable;
 import javax.persistence.*;
@@ -145,5 +144,3 @@ public class LicenseDetail implements Serializable {
 	}
 
 }
-
-//JIRA: SS-1: By RM06: EOC
