660224fa45742815e84447984fecf668e341d801
pandeyritwik <ritwik@raremile.com>
2014-11-18 18:34:10 +0530

    Formatted GenericDao and GenericDaoImpl. RM-05

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/impl/GenericDaoImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/impl/GenericDaoImpl.java
@@ -16,18 +16,19 @@ import com.realtech.socialsurvey.core.dao.GenericDao;
 //JIRA: SS-8: By RM05: BOC
 
 /**
- * This is the base Dao which needs to be extended by each Dao.
- * It contains implementation for basic CRUD methods required by every Dao.
+ * This is the base Dao which needs to be extended by each Dao. It contains
+ * implementation for basic CRUD methods required by every Dao.
  */
-public class GenericDaoImpl<T, ID extends Serializable> implements GenericDao<T, ID> {
+public class GenericDaoImpl<T, ID extends Serializable> implements
+		GenericDao<T, ID> {
 
-	private Class<T> persistentClass;  
-	private SessionFactory sessionFactory; 
+	private Class<T> persistentClass;
+	private SessionFactory sessionFactory;
 	private Session session;
 
-	public void setSession(Session s) {  
-		this.session = s;  
-	}  
+	public void setSession(Session s) {
+		this.session = s;
+	}
 
 	public SessionFactory getSessionFactory() {
 		return sessionFactory;
@@ -38,94 +39,94 @@ public class GenericDaoImpl<T, ID extends Serializable> implements GenericDao<T,
 	}
 
 	@Transactional
-	protected Session getSession() {  
+	protected Session getSession() {
 		try {
-			if (session == null)  
-				session=sessionFactory.openSession();
+			if (session == null)
+				session = sessionFactory.openSession();
 		} catch (HibernateException e) {
 			throw e;
 		}
-		return session;  
-	}  
+		return session;
+	}
 
-	public Class<T> getPersistentClass() {  
-		return persistentClass;  
-	}  
+	public Class<T> getPersistentClass() {
+		return persistentClass;
+	}
 
 	@SuppressWarnings("unchecked")
-	public T findById(ID id) {  
-		T entity;  
+	public T findById(ID id) {
+		T entity;
 		try {
 			entity = (T) getSession().load(getPersistentClass(), id);
 		} catch (HibernateException e) {
 			throw e;
 		}
-		return entity;  
-	}  
+		return entity;
+	}
 
 	@Override
-	public List<T> findAll() {  
-		try{
+	public List<T> findAll() {
+		try {
 			return findByCriteria();
 		} catch (HibernateException e) {
 			throw e;
 		}
-	}  
-	
-	@Override
-	@SuppressWarnings("unchecked")  
-	public List<T> findByExample(T exampleInstance, String[] excludeProperty) {  
-		Criteria crit = getSession().createCriteria(getPersistentClass());  
-		Example example =  Example.create(exampleInstance);  
-		for (String exclude : excludeProperty) {  
-			example.excludeProperty(exclude);  
-		}  
-		crit.add(example);  
-		return crit.list();  
 	}
 
 	@Override
-	public T saveOrUpdate(T entity) {  
-		try{
+	@SuppressWarnings("unchecked")
+	public List<T> findByExample(T exampleInstance, String[] excludeProperty) {
+		Criteria crit = getSession().createCriteria(getPersistentClass());
+		Example example = Example.create(exampleInstance);
+		for (String exclude : excludeProperty) {
+			example.excludeProperty(exclude);
+		}
+		crit.add(example);
+		return crit.list();
+	}
+
+	@Override
+	public T saveOrUpdate(T entity) {
+		try {
 			getSession().saveOrUpdate(entity);
 		} catch (HibernateException e) {
 			throw e;
 		}
-		return entity;  
-	}  
+		return entity;
+	}
 
 	@Override
 	@Transactional
 	public T save(T entity) {
-		try{
+		try {
 			getSession().save(entity);
 		} catch (HibernateException e) {
 			throw e;
 		}
-		return entity;  
-	} 
+		return entity;
+	}
 
 	@Override
 	public void delete(T entity) {
-		try{
+		try {
 			getSession().delete(entity);
 		} catch (HibernateException e) {
 			throw e;
 		}
-	}  
+	}
 
 	@Override
-	public void flush() {  
-		try{
+	public void flush() {
+		try {
 			getSession().flush();
 		} catch (HibernateException e) {
 			throw e;
 		}
-	}  
+	}
 
 	@Override
-	public void clear() {  
-		try{
+	public void clear() {
+		try {
 			getSession().clear();
 		} catch (HibernateException e) {
 			throw e;
@@ -133,13 +134,13 @@ public class GenericDaoImpl<T, ID extends Serializable> implements GenericDao<T,
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")  
-	public List<T> findByCriteria(Criterion... criterion) {  
-		Criteria crit = getSession().createCriteria(getPersistentClass());  
-		for (Criterion c : criterion) {  
+	@SuppressWarnings("unchecked")
+	public List<T> findByCriteria(Criterion... criterion) {
+		Criteria crit = getSession().createCriteria(getPersistentClass());
+		for (Criterion c : criterion) {
 			crit.add(c);
-		}  
-		return crit.list();  
+		}
+		return crit.list();
 	}
 }
-//JIRA: SS-8: By RM05: EOC
+// JIRA: SS-8: By RM05: EOC
