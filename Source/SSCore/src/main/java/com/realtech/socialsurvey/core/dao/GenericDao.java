95a855e37ab59c62ba7ba747f7f8d74f5b81a4e0
pandeyritwik <ritwik@raremile.com>
2014-11-18 12:47:14 +0530

    Added exception handling and few more methods into interface. RM-05

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/GenericDao.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/GenericDao.java
@@ -3,6 +3,8 @@ package com.realtech.socialsurvey.core.dao;
 import java.io.Serializable;
 import java.util.List;
 
+import org.hibernate.criterion.Criterion;
+
 //JIRA: SS-8: By RM05: BOC
 
 /*This is the base Dao which needs to be implemented by each GenericDao.
@@ -13,10 +15,13 @@ import java.util.List;
 		  
 	    public T findById(ID id);
 	    public List<T> findAll();
-	    public T saveOrUpdate(T entity);  
+	    public T saveOrUpdate(T entity);
 	    public T save(T entity);
-	    public void delete(T entity);  
+	    public void delete(T entity);
+	    public List<T> findByExample(T exampleInstance, String[] excludeProperty);
 	    public void flush(); 
+	    public void clear();
+	    public List<T> findByCriteria(Criterion... criterion);
 	
 }
 
--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/impl/GenericDaoImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/impl/GenericDaoImpl.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Criterion;
@@ -18,16 +19,16 @@ import com.realtech.socialsurvey.core.dao.GenericDao;
  * It contains implementation for basic CRUD methods required by every Dao.
  */
 public class GenericDaoImpl<T, ID extends Serializable> implements GenericDao<T, ID> {
-	
+
 	private Class<T> persistentClass;  
-    private SessionFactory sessionFactory; 
-    private Session session;
-  
-    public void setSession(Session s) {  
-        this.session = s;  
-    }  
-  
-    public SessionFactory getSessionFactory() {
+	private SessionFactory sessionFactory; 
+	private Session session;
+
+	public void setSession(Session s) {  
+		this.session = s;  
+	}  
+
+	public SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
 
@@ -36,72 +37,108 @@ public class GenericDaoImpl<T, ID extends Serializable> implements GenericDao<T,
 	}
 
 	@Transactional
-    protected Session getSession() {  
-        if (session == null)  
-            session=sessionFactory.openSession();
-        return session;  
-    }  
-  
-    public Class<T> getPersistentClass() {  
-        return persistentClass;  
-    }  
+	protected Session getSession() {  
+		try {
+			if (session == null)  
+				session=sessionFactory.openSession();
+		} catch (HibernateException e) {
+			throw e;
+		}
+		return session;  
+	}  
 
-    @SuppressWarnings("unchecked")
-    public T findById(ID id) {  
-        T entity;  
-        entity = (T) getSession().load(getPersistentClass(), id);
-  
-        return entity;  
-    }  
-  
-    public List<T> findAll() {  
-        return findByCriteria();  
-    }  
-  
-    @SuppressWarnings("unchecked")  
-    public List<T> findByExample(T exampleInstance, String[] excludeProperty) {  
-        Criteria crit = getSession().createCriteria(getPersistentClass());  
-        Example example =  Example.create(exampleInstance);  
-        for (String exclude : excludeProperty) {  
-            example.excludeProperty(exclude);  
-        }  
-        crit.add(example);  
-        return crit.list();  
-    }  
-  
-    public T saveOrUpdate(T entity) {  
-        getSession().saveOrUpdate(entity);
-        return entity;  
-    }  
-  
-    @Transactional
-    public T save(T entity) {
-        getSession().save(entity);
-        return entity;  
-    } 
-    
-    public void delete(T entity) {
-        getSession().delete(entity);  
-    }  
-  
-    public void flush() {  
-        getSession().flush();  
-    }  
-  
-    public void clear() {  
-        getSession().clear();  
-    }  
-  
-    /** 
-     * Use this inside subclasses as a convenience method.
-     */  
-    @SuppressWarnings("unchecked")  
-    protected List<T> findByCriteria(Criterion... criterion) {  
-        Criteria crit = getSession().createCriteria(getPersistentClass());  
-        for (Criterion c : criterion) {  
-            crit.add(c);  
-        }  
-        return crit.list();  
-   }
+	public Class<T> getPersistentClass() {  
+		return persistentClass;  
+	}  
+
+	@SuppressWarnings("unchecked")
+	public T findById(ID id) {  
+		T entity;  
+		try {
+			entity = (T) getSession().load(getPersistentClass(), id);
+		} catch (HibernateException e) {
+			throw e;
+		}
+		return entity;  
+	}  
+
+	@Override
+	public List<T> findAll() {  
+		try{
+			return findByCriteria();
+		} catch (HibernateException e) {
+			throw e;
+		}
+	}  
+	
+	@Override
+	@SuppressWarnings("unchecked")  
+	public List<T> findByExample(T exampleInstance, String[] excludeProperty) {  
+		Criteria crit = getSession().createCriteria(getPersistentClass());  
+		Example example =  Example.create(exampleInstance);  
+		for (String exclude : excludeProperty) {  
+			example.excludeProperty(exclude);  
+		}  
+		crit.add(example);  
+		return crit.list();  
+	}
+
+	@Override
+	public T saveOrUpdate(T entity) {  
+		try{
+			getSession().saveOrUpdate(entity);
+		} catch (HibernateException e) {
+			throw e;
+		}
+		return entity;  
+	}  
+
+	@Override
+	@Transactional
+	public T save(T entity) {
+		try{
+			getSession().save(entity);
+		} catch (HibernateException e) {
+			throw e;
+		}
+		return entity;  
+	} 
+
+	@Override
+	public void delete(T entity) {
+		try{
+			getSession().delete(entity);
+		} catch (HibernateException e) {
+			throw e;
+		}
+	}  
+
+	@Override
+	public void flush() {  
+		try{
+			getSession().flush();
+		} catch (HibernateException e) {
+			throw e;
+		}
+	}  
+
+	@Override
+	public void clear() {  
+		try{
+			getSession().clear();
+		} catch (HibernateException e) {
+			throw e;
+		}
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")  
+	public List<T> findByCriteria(Criterion... criterion) {  
+		Criteria crit = getSession().createCriteria(getPersistentClass());  
+		for (Criterion c : criterion) {  
+			crit.add(c);
+		}  
+		return crit.list();  
+	}
 }
 //JIRA: SS-8: By RM05: EOC
