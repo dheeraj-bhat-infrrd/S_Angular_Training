4855631b4c73ce72f71e2bd5ba07f0cca6688969
Nishit Kannan <niskan@gmail.com>
2014-11-21 18:53:37 +0530

    Payment module skeleton

---
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/payment/Payment.java
@@ -0,0 +1,15 @@
+package com.realtech.socialsurvey.core.services.payment;
+
+import com.realtech.socialsurvey.core.entities.Company;
+import com.realtech.socialsurvey.core.entities.User;
+import com.realtech.socialsurvey.core.exception.NonFatalException;
+
+/**
+ * Handles the payment options for the application
+ *
+ */
+public interface Payment {
+	
+	public void subscribe(User user, Company company, int accountsMasterId, String planId, String nonce) throws NonFatalException;
+
+}
