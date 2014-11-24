0bc66b483c3fa9e70b1209f160035482208c4b24
Nishit Kannan <niskan@gmail.com>
2014-11-24 12:28:06 +0530

    Refactored

--- Source/SSCore/src/test/java/com/realtech/socialsurvey/core/services/generator/impl/UrlGeneratorImplTest.java
+++ Source/SSCore/src/test/java/com/realtech/socialsurvey/core/services/generator/impl/UrlGeneratorImplTest.java
@@ -10,8 +10,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.realtech.socialsurvey.core.exception.InvalidInputException;
