b3f265177d26ced2f066c1a26e796e5fd9a87020
pandeyritwik <ritwik@raremile.com>
2014-11-24 15:56:33 +0530

    Added name of table for company. RM-05

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/Company.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/Company.java
@@ -1,7 +1,9 @@
 package com.realtech.socialsurvey.core.entities;
 
 import java.io.Serializable;
+
 import javax.persistence.*;
+
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -11,6 +13,7 @@ import java.util.List;
  * 
  */
 @Entity
+@Table(name="company")
 @NamedQuery(name="Company.findAll", query="SELECT c FROM Company c")
 public class Company implements Serializable {
 	private static final long serialVersionUID = 1L;
