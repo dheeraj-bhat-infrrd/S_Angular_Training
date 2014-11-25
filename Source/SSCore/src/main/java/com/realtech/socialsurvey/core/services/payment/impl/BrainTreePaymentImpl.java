08201c9a1500031deb4076218924e5adf652b846
Nishit Kannan <niskan@gmail.com>
2014-11-25 11:31:28 +0530

    Registered payment implementation as Component

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/payment/impl/BrainTreePaymentImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/payment/impl/BrainTreePaymentImpl.java
@@ -1,10 +1,12 @@
 package com.realtech.socialsurvey.core.services.payment.impl;
 
+import org.springframework.stereotype.Component;
 import com.realtech.socialsurvey.core.entities.Company;
 import com.realtech.socialsurvey.core.entities.User;
 import com.realtech.socialsurvey.core.exception.NonFatalException;
 import com.realtech.socialsurvey.core.services.payment.Payment;
 
+@Component
 public class BrainTreePaymentImpl implements Payment {
 
 	@Override
