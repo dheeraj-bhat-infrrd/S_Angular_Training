aeb9f7dd7c3abd6d82fbe358d16c55e845f5a245
pandeyritwik <ritwik@raremile.com>
2014-11-25 18:18:11 +0530

    Updated name of tables to Uppercase for each entity. RM-05

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/Region.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/Region.java
@@ -11,6 +11,7 @@ import java.util.List;
  * 
  */
 @Entity
+@Table(name="REGION")
 @NamedQuery(name="Region.findAll", query="SELECT r FROM Region r")
 public class Region implements Serializable {
 	private static final long serialVersionUID = 1L;
