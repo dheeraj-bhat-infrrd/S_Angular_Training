455105c5804cba9756a49d884c6974a138a98849
bhumika2407 <niskan@gmail.com>
2014-11-27 15:25:18 +0530

    JIRA:SS-24 and SS-26 added jsp and controllers for registration flow

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/utils/DisplayMessageConstants.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/utils/DisplayMessageConstants.java
@@ -9,11 +9,20 @@ public final class DisplayMessageConstants {
 
 	private DisplayMessageConstants() {}
 
+	// error message constants
 	public static final String INVALID_FIRSTNAME = "INVALID_FIRSTNAME";
 	public static final String INVALID_LASTNAME = "INVALID_LASTNAME";
 	public static final String INVALID_EMAILID = "INVALID_EMAILID";
 	public static final String INVALID_CAPTCHA = "INVALID_CAPTCHA";
 	public static final String REGISTRATION_INVITE_GENERAL_ERROR = "REGISTRATION_INVITE_GENERAL_ERROR";
 	public static final String GENERAL_ERROR = "GENERAL_ERROR";
+	public static final String INVALID_USERNAME = "INVALID_USERNAME";
+	public static final String INVALID_PASSWORD = "INVALID_PASSWORD";
+	public static final String PASSWORDS_MISMATCH = "PASSWORDS_MISMATCH";
+	public static final String REGISTRATION_GENERAL_ERROR = "REGISTRATION_GENERAL_ERROR";
+	public static final String INVALID_REGISTRATION_URL = "INVALID_REGISTRATION_URL";
+
+	// success message constants
 	public static final String REGISTRATION_INVITE_SUCCESSFUL = "REGISTRATION_INVITE_SUCCESSFUL";
+	public static final String USER_REGISTRATION_SUCCESSFUL = "USER_REGISTRATION_SUCCESSFUL";
 }
