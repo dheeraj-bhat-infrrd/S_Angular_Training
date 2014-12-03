2e661f6673e8efe21b500fa4621f422f0c4051d4
agrawal-shubham <shubham@raremile.com>
2014-12-03 18:35:10 +0530

    JIRA ss-21 and ss-30 by RM-06 Changes for login module. Added new jsp for buildsurvey.jsp

--- Source/SSWeb/src/main/webapp/resources/js/loginScript.js
+++ Source/SSWeb/src/main/webapp/resources/js/loginScript.js
@@ -2,18 +2,35 @@
  *Contains all JavaScript for login purpose
  */
 
+/**
+ * 
+ */
 function loginUser() {
 	console.log("authenticate user");
-
+	var success = 0;
+	if (!$('#messageHeader').hasClass("hide"))
+		$('#messageHeader').addClass("hide");
 	var payLoad = $("#loginForm").serialize();
-	
+	var redirectTo = '';
+
 	$.ajax({
 		url : "./userlogin.do",
 		type : "POST",
 		data : payLoad,
 		success : function(data) {
-			alert("Success");
 			console.log(data);
+			$('#messageHeader').html(data);
+			if ($('#messageHeader').find('div').hasClass('success_message'))
+				success = 1;
+			$('#messageHeader').removeClass("hide");
+			redirectTo = $("#redirectUrl").val();
+
+		},
+		complete : function(data) {
+			if (success) {
+				console.log("Login Successful");
+				location.href = "./" + redirectTo;
+			}
 		}
 	});
 
