7c2d5653e8115073396615d9b941a509dc036de8
agrawal-shubham <shubham@raremile.com>
2014-12-04 21:00:20 +0530

    JIRA SS-35 by RM-06
    Services to fetch branch, region list
    also to delete a branch or region

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/hierarchymanagement/impl/HierarchyManagementServiceImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/hierarchymanagement/impl/HierarchyManagementServiceImpl.java
@@ -1,14 +1,127 @@
 package com.realtech.socialsurvey.core.services.hierarchymanagement.impl;
 
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import com.realtech.socialsurvey.core.commons.CommonConstants;
+import com.realtech.socialsurvey.core.dao.GenericDao;
+import com.realtech.socialsurvey.core.entities.Branch;
+import com.realtech.socialsurvey.core.entities.Company;
+import com.realtech.socialsurvey.core.entities.Region;
+import com.realtech.socialsurvey.core.exception.InvalidInputException;
 import com.realtech.socialsurvey.core.services.hierarchymanagement.HierarchyManagementServices;
-//JIRA SS-37 BY RM02 BOC
+
+// JIRA SS-37 BY RM02 BOC
 
 /**
  * Implementation class for Hierarchy management services
- * 
  */
-public class HierarchyManagementServiceImpl implements
-		HierarchyManagementServices {
+@Component
+public class HierarchyManagementServiceImpl implements HierarchyManagementServices {
+
+	private static final Logger LOG = LoggerFactory.getLogger(HierarchyManagementServiceImpl.class);
+
+	@Autowired
+	private GenericDao<Branch, Integer> branchDao;
+
+	@Autowired
+	private GenericDao<Region, Integer> regionDao;
+
+	private static final String COMPANY = "company";
+
+	/**
+	 * Fetch list of branches in a company
+	 * 
+	 * @param company
+	 * @return List of branches
+	 * @throws InvalidInputException
+	 */
+	@Override
+	@Transactional
+	public List<Branch> getAllBranchesForCompany(Company company) throws InvalidInputException {
+
+		if (company == null) {
+			LOG.error("Company object passed can not be null");
+			throw new InvalidInputException("Invalid Company passed");
+		}
+
+		LOG.info("Fetching the list of branches for company :" + company.getCompany());
+		List<Branch> branchList = branchDao.findByColumn(Branch.class, COMPANY, company);
+		LOG.info("Branch list fetched for the company " + company);
+		return branchList;
+	}
+
+	/**
+	 * Fetch list of regions in a company
+	 * 
+	 * @param company
+	 * @return List of regions
+	 * @throws InvalidInputException
+	 */
+	@Override
+	@Transactional
+	public List<Region> getAllRegionsForCompany(Company company) throws InvalidInputException {
+		if (company == null) {
+			LOG.error("Company object passed can not be null");
+			throw new InvalidInputException("Invalid Company passed");
+		}
+
+		LOG.info("Fetching the list of regions for company :" + company.getCompany());
+		List<Region> regionList = regionDao.findByColumn(Region.class, COMPANY, company);
+		LOG.info("Region list fetched for the company " + company);
+		return regionList;
+	}
+
+	/**
+	 * update branch status to inactive
+	 * 
+	 * @param BranchId
+	 * @throws InvalidInputException
+	 */
+	@Override
+	@Transactional
+	public void deleteBranch(int branchId) throws InvalidInputException {
+		LOG.info("Update branch status to inactive on delete");
+
+		LOG.debug("Fetch the branch object by ID");
+		Branch branch = branchDao.findById(Branch.class, branchId);
+		if (branch == null) {
+			LOG.error("No branch present with the branch Id :" + branchId);
+			throw new InvalidInputException("No branch present with the branch Id :" + branchId);
+		}
+
+		// set branch status as inactive
+		branch.setStatus(CommonConstants.STATUS_INACTIVE);
+		// update the branch status in database
+		branchDao.saveOrUpdate(branch);
+		LOG.info("Branch status for branch ID :" + branchId + "/t successfully updated to inacitive");
+	}
+
+	/**
+	 * update region status to inactive
+	 * 
+	 * @param regionId
+	 * @throws InvalidInputException
+	 */
+	@Override
+	@Transactional
+	public void deleteRegion(int regionId) throws InvalidInputException {
+		LOG.info("Update region status to inactive on delete");
+		LOG.debug("Fetch the branch object by ID");
+		Region region = regionDao.findById(Region.class, regionId);
+		if (region == null) {
+			LOG.error("No branch present with the region Id :" + regionId);
+			throw new InvalidInputException("No branch present with the region Id :" + regionId);
+		}
+		// set region status as inactive
+		region.setStatus(CommonConstants.STATUS_INACTIVE);
+		// update the region status in the database
+		regionDao.saveOrUpdate(region);
+		LOG.info("Branch status for region ID :" + regionId + "/t successfully updated to inacitive");
+	}
 
 }
-//JIRA SS-37 BY RM02 EOC
+// JIRA SS-37 BY RM02 EOC
--- Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/controller/HierarchyManagementController.java
+++ Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/controller/HierarchyManagementController.java
@@ -1,13 +1,23 @@
 package com.realtech.socialsurvey.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
-
+import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import com.realtech.socialsurvey.core.commons.CommonConstants;
+import com.realtech.socialsurvey.core.entities.User;
+import com.realtech.socialsurvey.core.enums.DisplayMessageType;
+import com.realtech.socialsurvey.core.exception.InvalidInputException;
+import com.realtech.socialsurvey.core.exception.NonFatalException;
+import com.realtech.socialsurvey.core.services.hierarchymanagement.HierarchyManagementServices;
+import com.realtech.socialsurvey.core.utils.DisplayMessageConstants;
+import com.realtech.socialsurvey.core.utils.MessageUtils;
+import com.realtech.socialsurvey.web.common.JspResolver;
 
 // JIRA SS-37 BY RM02 BOC
 /**
@@ -16,8 +26,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HierarchyManagementController {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(HierarchyManagementController.class);
+	private static final Logger LOG = LoggerFactory.getLogger(HierarchyManagementController.class);
+
+	@Autowired
+	private MessageUtils messageUtils;
+	@Autowired
+	HierarchyManagementServices hierarchyManagementServices;
 
 	/**
 	 * Method to call services for showing up the build hierarchy page
@@ -34,5 +48,96 @@ public class HierarchyManagementController {
 		return null;
 	}
 
+	/**
+	 * Fetch the list of branches for the company
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/fetchallbranches", method = RequestMethod.GET)
+	public String fetchAllBranches(Model model, HttpServletRequest request) {
+
+		LOG.info("Fetching all the branches for current user");
+		// get current session
+		HttpSession session = request.getSession(false);
+		// get current user in session
+		User user = (User) session.getAttribute(CommonConstants.USER_IN_SESSION);
+
+		// get the list of branches in company
+		try {
+			try {
+				hierarchyManagementServices.getAllBranchesForCompany(user.getCompany());
+			}
+			catch (InvalidInputException e) {
+				LOG.error("Error occurred while fetching the branch list in method getAllBranchesForCompany");
+				throw new InvalidInputException("Error occurred while fetching the branch list in method getAllBranchesForCompany",
+						DisplayMessageConstants.GENERAL_ERROR, e);
+			}
+		}
+		catch (NonFatalException e) {
+			LOG.error("NonFatalException while sending the reset password link. Reason : " + e.getMessage(), e);
+			model.addAttribute("message", messageUtils.getDisplayMessage(e.getErrorCode(), DisplayMessageType.ERROR_MESSAGE));
+		}
+		return JspResolver.MESSAGE_HEADER;
+	}
+
+	/**
+	 * Fetch the list of regions for the company
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/fetchallregions", method = RequestMethod.GET)
+	public String fetchAllRegions(Model model, HttpServletRequest request) {
+
+		LOG.info("Fetching all the regions for current user");
+		// get current session
+		HttpSession session = request.getSession(false);
+		// get current user in session
+		User user = (User) session.getAttribute(CommonConstants.USER_IN_SESSION);
+
+		// get the list of regions in company
+		try {
+			try {
+				hierarchyManagementServices.getAllRegionsForCompany(user.getCompany());
+			}
+			catch (InvalidInputException e) {
+				LOG.error("Error occurred while fetching the branch list in method getAllRegionsForCompany");
+				throw new InvalidInputException("Error occurred while fetching the region list in method getAllBranchesForCompany",
+						DisplayMessageConstants.GENERAL_ERROR, e);
+			}
+		}
+		catch (NonFatalException e) {
+			LOG.error("NonFatalException while sending the reset password link. Reason : " + e.getMessage(), e);
+			model.addAttribute("message", messageUtils.getDisplayMessage(e.getErrorCode(), DisplayMessageType.ERROR_MESSAGE));
+		}
+		return JspResolver.MESSAGE_HEADER;
+	}
+	
+	/**
+	 * Deactivates a region status
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/deactivateregion", method = RequestMethod.POST)
+	public String decatvieRegion(Model model, HttpServletRequest request){
+		LOG.info("Deactive region");
+		return JspResolver.MESSAGE_HEADER;
+	}
+	
+	/**
+	 * Deactivates a region status
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/deactivatebranch", method = RequestMethod.POST)
+	public String decatvieBranch(Model model, HttpServletRequest request){
+		LOG.info("Deactive region");
+		return JspResolver.MESSAGE_HEADER;
+	}
+
+	
 }
 // JIRA SS-37 BY RM02 EOC
