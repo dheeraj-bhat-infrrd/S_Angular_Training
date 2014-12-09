f9baa39739b1a919748b2611890d4d39ab9167af
bhumika2407 <bhumika@raremile.com>
2014-12-09 16:17:40 +0530

    JIRA:SS-37 coded for hierarchy management.Added a temporary link in dashboard

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/UserProfileDao.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/UserProfileDao.java
@@ -7,5 +7,5 @@ import com.realtech.socialsurvey.core.entities.UserProfile;
 public interface UserProfileDao extends GenericDao<UserProfile, Long> {
 
 	public void createUserProfile(User user, Company company, String emailId, long agentId, long branchId, long regionId, int profileMasterId,
-			String profileCompletionStage, int isProfileComplete);
+			String profileCompletionStage, int isProfileComplete,String createdBy, String modifiedBy);
 }
