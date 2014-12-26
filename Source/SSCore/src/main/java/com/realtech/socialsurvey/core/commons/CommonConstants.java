ee74d8c14bb31e0a39ce5a3f79ded09c292c711b
pandeyritwik <ritwik@raremile.com>
2014-12-26 12:46:59 +0530

    Resolved Merge Conflicts.

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/commons/CommonConstants.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/commons/CommonConstants.java
@@ -14,11 +14,11 @@ public interface CommonConstants {
 	public static final String SENDGRID_SENDER_NAME = "SENDGRID_SENDER_NAME";
 	public static final String SENDGRID_SENDER_PASSWORD = "SENDGRID_SENDER_PASSWORD";
 
-	
 	/**
 	 * Default constants
 	 */
-	// default company id for application. if any entity is linked to this id, then its an orphan entity
+	// default company id for application. if any entity is linked to this id, then its an orphan
+	// entity
 	public static final long DEFAULT_COMPANY_ID = 1;
 	public static final String DEFAULT_BRANCH_NAME = "Defult Branch";
 	public static final String DEFAULT_REGION_NAME = "Defult Region";
@@ -35,26 +35,31 @@ public interface CommonConstants {
 	public static final int PROFILES_MASTER_REGION_ADMIN_PROFILE_ID = 3;
 	public static final int PROFILES_MASTER_BRANCH_ADMIN_PROFILE_ID = 4;
 	public static final int PROFILES_MASTER_AGENT_PROFILE_ID = 5;
-	
+
 	/**
 	 * Profile completion stages constants and form action constants, store the url mappings
 	 */
 	public static final String ADD_COMPANY_STAGE = "addcompanyinformation.do";
 	public static final String ADD_ACCOUNT_TYPE_STAGE = "addaccounttype.do";
-	public static final String PAYMENT_STAGE = "payment.do";
 	public static final String RESET_PASSWORD = "resetpassword.do";
-	public static final String LOGIN_STAGE = "dashboard.do";
+	public static final String DASHBOARD_STAGE = "dashboard.do";
 	public static final String PROFILE_STAGES_COMPLETE = "complete";
+	public static final String REQUEST_MAPPING_SHOW_REGISTRATION = "showregistrationpage.do";
+	public static final String REQUEST_MAPPING_MAIL_VERIFICATION = "verification.do";
 
 	/**
 	 * Status constants
 	 */
 	public static final int STATUS_ACTIVE = 1;
 	public static final int STATUS_INACTIVE = 0;
+	public static final int STATUS_NOT_VERIFIED = 2;
 	public static final int PROCESS_COMPLETE = 1;
 	public static final int PROCESS_NOT_STARTED = 0;
 	public static final int IS_DEFAULT_BY_SYSTEM_YES = 1;
 	public static final int IS_DEFAULT_BY_SYSTEM_NO = 0;
+	public static final int SANDBOX_MODE_TRUE=1;
+	public static final int IS_OWNER = 1;
+	public static final int IS_NOT_OWNER = 0;
 
 	/**
 	 * Hibernate entities and column name constants
@@ -64,23 +69,32 @@ public interface CommonConstants {
 	public static final String INVITATION_EMAIL_ID_COLUMN = "invitationEmailId";
 	public static final String USER_LOGIN_NAME_COLUMN = "loginName";
 	public static final String USER_INVITE_INVITATION_VALID_UNTIL_COLUMN = "invitationValidUntil";
-	public static final String REQUEST_MAPPING_SHOW_REGISTRATION = "showregistrationpage.do";
 	public static final String USER_COLUMN = "user";
 	public static final String PROFILE_MASTER_COLUMN = "profilesMaster";
 	public static final String AUTO_PAYMENT_MODE = "A";
 	public static final int INITIAL_PAYMENT_RETRIES = 0;
+	public static final String PAYMENT_GATEWAY = "Braintree";
+	public static final String IS_DEFAULT_BY_SYSTEM = "isDefaultBySystem";
+	public static final String COMPANY_COLUMN = "company";
 	public static final String SURVEY_QUESTION_COLUMN = "surveyQuestion";
 	public static final String SURVEY_COLUMN = "survey";
 	public static final String SURVEY_COMPANY_COLUMN = "company";
-
+	
 	/**
 	 * Constants to be used in code for referencing variables(i.e in maps or session attributes)
 	 */
 	public static final String USER_IN_SESSION = "user";
+	public static final String ACCOUNT_TYPE_IN_SESSION = "accounttype";
 	public static final String COMPANY_NAME = "companyName";
 	public static final String ADDRESS = "address";
 	public static final String ZIPCODE = "zipCode";
 	public static final String COMPANY_CONTACT_NUMBER = "companyContactNo";
+	public static final String COMPANY = "company";
+	public static final String EMAIL_ID = "emailId";
+	public static final String FIRST_NAME = "firstName";
+	public static final String LAST_NAME = "lastName";
+	public static final String USER_ID = "userId";
+	
 
 	/**
 	 * other constants
@@ -88,4 +102,18 @@ public interface CommonConstants {
 	public static final long EPOCH_TIME_IN_MILLIS = 1000l;
 	public static final String GUEST_USER_NAME = "GUEST";
 	public static final String ADMIN_USER_NAME = "ADMIN";
+	public static final int MAX_BRANCH_LIMIT_TEAM = 1;
+	public static final int NO_LIMIT = -1;
+	public static final int MAX_REGION_LIMIT_COMPANY = 1;
+	public static final int INITIAL_INDEX = 0;
+
+	/**
+	 * Logo related config
+	 */
+	public static final String MAX_LOGO_SIZE_BYTES = "MAX_LOGO_SIZE_BYTES";
+	public static final String MAX_LOGO_WIDTH_PIXELS = "MAX_LOGO_WIDTH_PIXELS";
+	public static final String MAX_LOGO_HEIGHT_PIXELS = "MAX_LOGO_HEIGHT_PIXELS";
+	public static final String LIST_LOGO_FORMATS = "LIST_LOGO_FORMATS";
+	public static final String LOGO_HOME_DIRECTORY = "LOGO_HOME_DIRECTORY";
+	public static final String LOGO_NAME = "logoName";
 }
