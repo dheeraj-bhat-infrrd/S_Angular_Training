be7660b9f39681388d7b924f9c57df913beba7ff
aswinsegu <aswin.segu@raremile.com>
2014-12-29 10:56:06 +0530

    Adde controller class

--- Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/controller/SurveyBuilderController.java
+++ Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/controller/SurveyBuilderController.java
@@ -1,6 +1,7 @@
 package com.realtech.socialsurvey.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import com.realtech.socialsurvey.core.commons.CommonConstants;
+import com.realtech.socialsurvey.core.entities.User;
 import com.realtech.socialsurvey.core.services.surveybuilder.SurveyBuilder;
 import com.realtech.socialsurvey.core.utils.MessageUtils;
 
@@ -22,10 +25,34 @@ public class SurveyBuilderController {
 	@Autowired
 	private SurveyBuilder surveyBuilder;
 
-	@RequestMapping(value = "/corporateinvite", method = RequestMethod.POST)
-	public String addNewSurvey(Model model, HttpServletRequest request) {
+	@RequestMapping(value = "/createsurvey", method = RequestMethod.POST)
+	public String createNewSurvey(Model model, HttpServletRequest request) {
 		LOG.info("Method addNewSurvey of SurveyBuilderController called");
+		HttpSession session = request.getSession(false);
+		User user = (User) session.getAttribute(CommonConstants.USER_IN_SESSION);
+		String surveyName = request.getParameter("surveyName");
+		
+		//surveyBuilder.createNewSurvey(user, surveyQuestions, surveyName);
+		LOG.info("Method addNewSurvey of SurveyBuilderController finished successfully");
+		return null;
+	}
 
+	@RequestMapping(value = "/addsurveytocompany", method = RequestMethod.POST)
+	public String addSurveyToCompany(Model model, HttpServletRequest request) {
+		LOG.info("Method addNewSurvey of SurveyBuilderController called");
+		HttpSession session = request.getSession(false);
+		User user = (User) session.getAttribute(CommonConstants.USER_IN_SESSION);
+		
+		LOG.info("Method addNewSurvey of SurveyBuilderController finished successfully");
+		return null;
+	}
+
+	@RequestMapping(value = "/addquestionstosurvey", method = RequestMethod.POST)
+	public String addQuestionsToExistingSurvey(Model model, HttpServletRequest request) {
+		LOG.info("Method addNewSurvey of SurveyBuilderController called");
+		HttpSession session = request.getSession(false);
+		User user = (User) session.getAttribute(CommonConstants.USER_IN_SESSION);
+		
 		LOG.info("Method addNewSurvey of SurveyBuilderController finished successfully");
 		return null;
 	}
