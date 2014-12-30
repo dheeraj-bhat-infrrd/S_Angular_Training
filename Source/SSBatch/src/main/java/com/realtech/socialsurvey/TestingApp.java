083b46fd835ee4a3588cfd35cc4ea7de19d30e69
karthikraremile <karthik.srivatsa@raremile.com>
2014-12-30 18:53:09 +0530

    Split the datasources between batch and web

--- Source/SSBatch/src/main/java/com/realtech/socialsurvey/TestingApp.java
+++ Source/SSBatch/src/main/java/com/realtech/socialsurvey/TestingApp.java
@@ -1,21 +1,17 @@
 package com.realtech.socialsurvey;
-//JIRA: SS-61: By RM03
+
+// JIRA: SS-61: By RM03
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import com.realtech.socialsurvey.core.commons.InitializeJNDI;
- 
-public class TestingApp {
-	
 
-    @SuppressWarnings("resource")
-    public static void main(String areg[]) throws Exception{
-         
-		InitializeJNDI.initializeJNDIforTest();
-    	
-        @SuppressWarnings("unused")
-		ApplicationContext context = new ClassPathXmlApplicationContext("launch-context.xml");
-         
-    }
- 
+public class TestingApp {
+
+	@SuppressWarnings("resource")
+	public static void main(String areg[]) throws Exception {
+
+		@SuppressWarnings("unused") ApplicationContext context = new ClassPathXmlApplicationContext("launch-context.xml");
+
+	}
+
 }
