f7a3627a66abba44cb5d6ed06a7e6a4fb3fc1037
agrawal-shubham <shubham@raremile.com>
2015-01-02 16:08:00 +0530

    JIRA SS-62 by RM-06
    1. Added backend firstname, lastname, companyname, password validation.
    2. Stored all regular expressions in CommonConstant.java.

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/commons/CommonConstants.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/commons/CommonConstants.java
@@ -123,4 +123,14 @@ public interface CommonConstants {
 	// mail content
 	public static final String SURVEY_MAIL_BODY_CATEGORY = "SURVEY_MAIL_BODY_CATEGORY";
 	public static final String SURVEY_REMINDER_MAIL_BODY_CATEGORY = "SURVEY_REMINDER_MAIL_BODY_CATEGORY";
+	
+	//regular expressions
+	public static final String PASSWORD_REG_EX = "^(?=.*[a-zA-Z0-9])(?=.*[!@#$%&*()_+=|<>?{}~-]).{6,15}$";
+	public static final String PHONENUMBER_REGEX = "^((\\+)|(00)|(\\*)|())[0-9]{3,14}((\\#)|())$";
+	public static final String ZIPCODE_REGEX = "\\d{5}(-\\d{4})?";
+	public static final String COMPANY_NAME_REGEX = "^[a-zA-Z0-9 ]$";
+	public static final String EMAIL_REGEX = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+	public static final String FIRST_NAME_REGEX = "[a-zA-Z]+";
+	public static final String LAST_NAME_REGEX = "[a-zA-Z ]+";
+	
 }
