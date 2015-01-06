a39e84ad40656d754068fbd3bf9d6e40c06e5f17
Nishit Kannan <niskan@gmail.com>
2015-01-06 19:33:44 +0530

    Merged code

--- Source/SSCore/src/test/java/com/realtech/socialsurvey/core/commons/SpringContextRule.java
+++ Source/SSCore/src/test/java/com/realtech/socialsurvey/core/commons/SpringContextRule.java
@@ -3,15 +3,20 @@ package com.realtech.socialsurvey.core.commons;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringContextRule implements TestRule {
 
+	private static final Logger LOG = LoggerFactory.getLogger(SpringContextRule.class);
+	
 	private static boolean isSpringContextLoaded = false;
 	// array of classpath context
 	private final String[] locations;
 	private final Object target;
+	private ConfigurableApplicationContext context;
 
 	public SpringContextRule(String[] locations, Object target) {
 		this.locations = locations;
@@ -25,19 +30,29 @@ public class SpringContextRule implements TestRule {
 			@Override
 			public void evaluate() throws Throwable {
 				if (!isSpringContextLoaded) {
-					ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(locations);
-					context.getAutowireCapableBeanFactory().autowireBean(target);
-					context.start();
+					loadContext();
 					try {
 						base.evaluate();
 					}
 					finally {
-						context.close();
+						closeContext();
 					}
 					isSpringContextLoaded = true;
 				}
 			}
 		};
+	}
+	
+	public void loadContext(){
+		LOG.info("Loading context from location");
+		context = new ClassPathXmlApplicationContext(locations);
+		context.getAutowireCapableBeanFactory().autowireBean(target);
+		context.start();
+	}
+	
+	public void closeContext(){
+		LOG.info("Closing context");
+		context.close();
 	}
 
 }
