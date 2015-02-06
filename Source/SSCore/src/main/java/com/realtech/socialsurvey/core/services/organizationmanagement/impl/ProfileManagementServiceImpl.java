7428d0c5aec7a2ac33e7f51be7244e50dc5d214d
aswinsegu <aswin.segu@raremile.com>
2015-02-06 12:42:37 +0530

    Checking in all the changes

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/organizationmanagement/impl/ProfileManagementServiceImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/organizationmanagement/impl/ProfileManagementServiceImpl.java
@@ -1,5 +1,6 @@
 package com.realtech.socialsurvey.core.services.organizationmanagement.impl;
 
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
@@ -9,6 +10,7 @@ import com.realtech.socialsurvey.core.entities.AgentSettings;
 import com.realtech.socialsurvey.core.entities.LockSettings;
 import com.realtech.socialsurvey.core.entities.OrganizationUnitSettings;
 import com.realtech.socialsurvey.core.entities.User;
+import com.realtech.socialsurvey.core.entities.UserProfile;
 import com.realtech.socialsurvey.core.entities.UserSettings;
 import com.realtech.socialsurvey.core.enums.AccountType;
 import com.realtech.socialsurvey.core.exception.InvalidInputException;
@@ -38,7 +40,11 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
 			throw new InvalidInputException("Invalid account type.");
 		}
 
+		List<UserProfile> userProfiles = user.getUserProfiles();
 		OrganizationUnitSettings finalSettings = null;
+		long agentId;
+		long branchId;
+		long regionId;
 		switch (accountType) {
 			case INDIVIDUAL:
 			case TEAM:
@@ -50,7 +56,8 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
 
 				// Individual
 				else if (user.isAgent()) {
-					finalSettings = generateAgentProfile(settings.getCompanySettings(), null, null, settings.getAgentSettings().get(user.getUserId()));
+					agentId = userProfiles.get(0).getAgentId();
+					finalSettings = generateAgentProfile(settings.getCompanySettings(), null, null, settings.getAgentSettings().get(agentId));
 				}
 				break;
 
@@ -63,12 +70,16 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
 
 				// Branch Admin
 				else if (user.isBranchAdmin()) {
-					// finalSettings = settings.getBranchSettings().get(branchId);
+					branchId = userProfiles.get(0).getBranchId();
+					finalSettings = generateBranchProfile(settings.getCompanySettings(), null, settings.getBranchSettings().get(branchId));
 				}
 
 				// Individual
 				else if (user.isAgent()) {
-					// finalSettings = settings.getAgentSettings().get(user.getUserId());
+					agentId = userProfiles.get(0).getAgentId();
+					branchId = userProfiles.get(0).getBranchId();
+					finalSettings = generateAgentProfile(settings.getCompanySettings(), null, settings.getBranchSettings().get(branchId), settings
+							.getAgentSettings().get(agentId));
 				}
 				break;
 
@@ -81,17 +92,25 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
 
 				// Region Admin
 				else if (user.isRegionAdmin()) {
-					// finalSettings = settings.getRegionSettings().get(regionId);
+					regionId = userProfiles.get(0).getRegionId();
+					finalSettings = generateRegionProfile(settings.getCompanySettings(), settings.getRegionSettings().get(regionId));
 				}
 
 				// Branch Admin
 				else if (user.isBranchAdmin()) {
-					// finalSettings = settings.getBranchSettings().get(branchId);
+					branchId = userProfiles.get(0).getBranchId();
+					regionId = userProfiles.get(0).getRegionId();
+					finalSettings = generateBranchProfile(settings.getCompanySettings(), settings.getRegionSettings().get(regionId), settings
+							.getBranchSettings().get(branchId));
 				}
 
 				// Individual
 				else if (user.isAgent()) {
-					// finalSettings = settings.getAgentSettings().get(user.getUserId());
+					agentId = userProfiles.get(0).getAgentId();
+					branchId = userProfiles.get(0).getBranchId();
+					regionId = userProfiles.get(0).getRegionId();
+					finalSettings = generateAgentProfile(settings.getCompanySettings(), settings.getRegionSettings().get(regionId), settings
+							.getBranchSettings().get(branchId), settings.getAgentSettings().get(agentId));
 				}
 				break;
 
@@ -101,6 +120,39 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
 
 		LOG.info("Method finalizeProfileDetail() finished from ProfileManagementService");
 		return finalSettings;
+	}
+
+	private OrganizationUnitSettings generateRegionProfile(OrganizationUnitSettings companySettings, OrganizationUnitSettings regionSettings)
+			throws InvalidInputException {
+		if (companySettings == null || regionSettings == null) {
+			throw new InvalidInputException("No Settings found");
+		}
+
+		// Company Lock settings
+		LockSettings regionLock = new LockSettings();
+		updateSettings(companySettings, regionSettings, regionLock);
+
+		regionSettings.setLockSettings(regionLock);
+		return regionSettings;
+	}
+
+	private OrganizationUnitSettings generateBranchProfile(OrganizationUnitSettings companySettings, OrganizationUnitSettings regionSettings,
+			OrganizationUnitSettings branchSettings) throws InvalidInputException {
+		if (companySettings == null || branchSettings == null) {
+			throw new InvalidInputException("No Settings found");
+		}
+
+		// Company Lock settings
+		LockSettings branchLock = new LockSettings();
+		updateSettings(companySettings, branchSettings, branchLock);
+
+		// Region Lock settings
+		if (regionSettings != null) {
+			updateSettings(regionSettings, branchSettings, branchLock);
+		}
+
+		branchSettings.setLockSettings(branchLock);
+		return branchSettings;
 	}
 
 	private AgentSettings generateAgentProfile(OrganizationUnitSettings companySettings, OrganizationUnitSettings regionSettings,
@@ -127,7 +179,7 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
 		return agentSettings;
 	}
 
-	private void updateSettings(OrganizationUnitSettings settings, AgentSettings agentSettings, LockSettings agentLock) {
+	private void updateSettings(OrganizationUnitSettings settings, OrganizationUnitSettings agentSettings, LockSettings agentLock) {
 		LockSettings lock = settings.getLockSettings();
 		if (lock != null) {
 			if (lock.isLogoLocked() && !agentLock.isLogoLocked()) {
