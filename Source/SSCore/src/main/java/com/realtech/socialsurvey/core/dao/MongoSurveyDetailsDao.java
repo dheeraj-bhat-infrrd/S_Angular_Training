ab014c2602785e27eb6d3e14e711563c7d1033cf
ritwik <ritwik@raremile.com>
2015-02-11 19:49:22 +0530

    Changes to 1. Check if answer stored is not number instead of undefined. 2. Aggregation queries for getting details on dashboard. BY RM-05

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/MongoSurveyDetailsDao.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/MongoSurveyDetailsDao.java
@@ -1,5 +1,6 @@
 package com.realtech.socialsurvey.core.dao;
 
+import java.util.Map;
 import com.realtech.socialsurvey.core.entities.SurveyDetails;
 import com.realtech.socialsurvey.core.entities.SurveyResponse;
 
@@ -14,5 +15,51 @@ public interface MongoSurveyDetailsDao {
 	public void updateGatewayAnswer(long agentId, String customerEmail, String mood, String review);
 
 	public void updateFinalScore(long agentId, String customerEmail);
+
+	public double getRatingOfAgentForPastNdays(long agentId, int noOfDays);
+
+	public Map<String, Long> getCountOfCustomersByMood();
+
+	public Map<String, Long> getCountOfCustomersByMoodForAgent(long agentId);
+
+	public Map<String, Long> getCountOfCustomersByMoodForBranch(long branchId);
+
+	public Map<String, Long> getCountOfCustomersByMoodForRegion(long regionId);
+
+	public Map<String, Long> getCountOfCustomersByMoodForCompany(long companyId);
+
+	public Map<String, Long> getCountOfCustomersByReminderMails();
+
+	public Map<String, Long> getCountOfCustomersByReminderMailsForAgent(long agentId);
+
+	public Map<String, Long> getCountOfCustomersByReminderMailsForBranch(long branchId);
+
+	public Map<String, Long> getCountOfCustomersByReminderMailsForRegion(long regionId);
+
+	public Map<String, Long> getCountOfCustomersByReminderMailsForCompany(long companyId);
+
+	public Map<String, Long> getCountOfCustomersByStage();
+
+	public Map<String, Long> getCountOfCustomersByStageForAgent(long agentId);
+
+	public Map<String, Long> getCountOfCustomersByStageForBranch(long branchId);
+
+	public Map<String, Long> getCountOfCustomersByStageForRegion(long regionId);
+
+	public Map<String, Long> getCountOfCustomersByStageForCompany(long companyId);
+
+	public long getTotalSurveyCountByMonth(int year, int month);
+
+	public Map<String, Long> getSocialPostsCount();
+
+	public Map<String, Long> getSocialPostsCountForAgent(long agentId);
+
+	public Map<String, Long> getSocialPostsCountForBranch(long branchId);
+
+	public Map<String, Long> getSocialPostsCountForRegion(long regionId);
+
+	public Map<String, Long> getSocialPostsCountForCompany(long companyId);
+
+	public Map<String, Long> getCountOfSurveyInitiators(String columnName, long columnValue);
 
 }
