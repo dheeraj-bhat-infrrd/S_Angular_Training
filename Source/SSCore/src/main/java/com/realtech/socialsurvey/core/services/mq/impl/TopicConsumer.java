752dc88daf6893f41f05a1a9c98635acee763227
Nishit Kannan <niskan@gmail.com>
2015-03-02 12:18:50 +0530

    Topic consumer code

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/mq/impl/TopicConsumer.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/mq/impl/TopicConsumer.java
@@ -1,12 +1,12 @@
 package com.realtech.socialsurvey.core.services.mq.impl;
 
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.realtech.socialsurvey.core.enums.EmailHeader;
 import com.realtech.socialsurvey.core.exception.NonFatalException;
 import com.realtech.socialsurvey.core.services.mq.InvalidMessageFormatException;
-import kafka.consumer.ConsumerIterator;
-import kafka.consumer.KafkaStream;
 
 /**
  * Consumes from a Kaafka topic
@@ -17,6 +17,8 @@ public class TopicConsumer implements Runnable {
 	public static final Logger LOG = LoggerFactory.getLogger(TopicConsumer.class);
 	
 	private static final String HEADER_MARKER = "HEADER^^";
+	private static final String RECIPIENT_MARKER = "RECIPIENT^^";
+	private static final String URL_MARKER = "URL^^";
 	private static final String ELEMENTS_DELIMITER = "$$";
 	
 	private KafkaStream<byte[], byte[]> stream;
@@ -51,14 +53,30 @@ public class TopicConsumer implements Runnable {
 		if(message.indexOf(HEADER_MARKER) == -1){
 			throw new InvalidMessageFormatException("Invalid format of the message. No header found in the message.");
 		}
-		String header = message.substring((HEADER_MARKER.length()), message.indexOf(ELEMENTS_DELIMITER));
+		String header = message.substring(HEADER_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER));
 		LOG.debug("Header: "+header);
 		return header;
 	}
 	
-	private void delegateProcess(String header, String message){
+	private void delegateProcess(String header, String message) throws InvalidMessageFormatException{
 		LOG.debug("Delegating message: "+message+" for header: "+header);
 		// delegate the mail according to the header
+		if(header.equals(EmailHeader.REGISTRATION.getName())){
+			parseRegistrationMailMessage(message);
+		}
+	}
+	
+	private void parseRegistrationMailMessage(String message) throws InvalidMessageFormatException{
+		if(message.indexOf(RECIPIENT_MARKER) == -1){
+			throw new InvalidMessageFormatException("Invalid format. Recipient is not present in the message for registration mail");
+		}
+		String recipient = message.substring(RECIPIENT_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER));
+		LOG.debug("Recipient: "+recipient);
+		int messageParsedIndex = RECIPIENT_MARKER.length()+recipient.length()+ELEMENTS_DELIMITER.length(); // holds the index till the message has been parsed.
+		String url = message.substring(messageParsedIndex, message.indexOf(ELEMENTS_DELIMITER, messageParsedIndex));
+		LOG.debug("Url: "+url);
+		messageParsedIndex+=URL_MARKER.length()+url.length()+ELEMENTS_DELIMITER.length();
+		
 		
 	}
 
