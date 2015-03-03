fac93dabf2f0cbcbce064937530bac0adb94a6fd
Nishit Kannan <niskan@gmail.com>
2015-03-03 17:15:11 +0530

    Implementation of application context

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/mq/impl/TopicConsumer.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/mq/impl/TopicConsumer.java
@@ -4,8 +4,10 @@ import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import com.realtech.socialsurvey.core.enums.EmailHeader;
 import com.realtech.socialsurvey.core.exception.NonFatalException;
+import com.realtech.socialsurvey.core.services.mail.EmailServices;
 import com.realtech.socialsurvey.core.services.mq.InvalidMessageFormatException;
 
 /**
@@ -20,20 +22,21 @@ public class TopicConsumer implements Runnable {
 	private static final String RECIPIENT_MARKER = "RECIPIENT^^";
 	private static final String URL_MARKER = "URL^^";
 	private static final String NAME_MARKER = "NAME^^";
+	private static final String FIRSTNAME_MARKER = "FIRSTNAME^^";
+	private static final String LASTNAME_MARKER = "LASTNAME^^";
+	private static final String RETRYDAYS_MARKER = "RETRYDAYS^^";
+	private static final String RETRIES_MARKER = "RETRIES^^";
+	private static final String AGENTNAME_MARKER = "AGENTNAME^^";
 	private static final String ELEMENTS_DELIMITER = "$$";
 	
 	private KafkaStream<byte[], byte[]> stream;
+	private EmailServices emailServices;
 	
-	public TopicConsumer(){}
-	
-	public TopicConsumer(KafkaStream<byte[], byte[]> stream){
+	public TopicConsumer(KafkaStream<byte[], byte[]> stream, ApplicationContext ctx){
 		this.stream = stream;
+		emailServices = ctx.getBean(EmailServices.class);
 	}
 	
-	public void setStream(KafkaStream<byte[], byte[]> stream){
-		this.stream = stream;
-	}
-
 	@Override
 	public void run() {
 		// iterate the stream
@@ -50,6 +53,7 @@ public class TopicConsumer implements Runnable {
 					delegateProcess(header, message);
 				}catch(NonFatalException nfe){
 					LOG.error("Could not process message: "+message, nfe);
+					// TODO: handle failed messages
 				}
 			}
 		}else{
@@ -69,33 +73,56 @@ public class TopicConsumer implements Runnable {
 		return header;
 	}
 	
-	private void delegateProcess(String header, String message) throws InvalidMessageFormatException{
+	private void delegateProcess(String header, String message) throws NonFatalException{
 		LOG.debug("Delegating message: "+message+" for header: "+header);
 		// delegate the mail according to the header
 		if(header.equals(EmailHeader.REGISTRATION.getName())){
 			parseRegistrationMailMessage(message);
 		}else if(header.equals(EmailHeader.VERFICATION.getName())){
-			parseVerificationMail(message);
+			parseMailMessage(message, EmailHeader.VERFICATION);
+		}else if(header.equals(EmailHeader.RESET_PASSWORD.getName())){
+			parseMailMessage(message, EmailHeader.RESET_PASSWORD);
+		}else if(header.equals(EmailHeader.SUBSCRIPTION_CHARGE_UNSUCESSFUL)){
+			parseSubscriptionChargeUnsucessfulMail(message);
+		}else if(header.equals(EmailHeader.REGISTRATION_COMPLETE.getName())){
+			parseMailMessage(message, EmailHeader.REGISTRATION_COMPLETE);
+		}else if(header.equals(EmailHeader.RETRY_CHARGE.getName())){
+			parseRetryChargeMail(message);
+		}else if(header.equals(EmailHeader.RETRY_EXHAUSTED.getName())){
+			parseMailWithRecipientAndName(message, EmailHeader.RETRY_EXHAUSTED);
+		}else if(header.equals(EmailHeader.ACCOUNT_DISABLED.getName())){
+			parseMailWithRecipientAndName(message, EmailHeader.ACCOUNT_DISABLED);
+		}else if(header.equals(EmailHeader.ACCOUNT_UPGRADE.getName())){
+			parseMailWithRecipientAndName(message, EmailHeader.ACCOUNT_UPGRADE);
+		}else if(header.equals(EmailHeader.SURVEY_COMPLETION.getName())){
+			parseSurveyCompletionMail(message);
 		}
 	}
 	
-	private void parseRegistrationMailMessage(String message) throws InvalidMessageFormatException{
-		if(message.indexOf(RECIPIENT_MARKER) == -1){
-			throw new InvalidMessageFormatException("Invalid format. Recipient is not present in the message for registration mail");
+	private void parseRegistrationMailMessage(String message) throws NonFatalException{
+		LOG.debug("Registration mail message: "+message);
+		if(message.indexOf(RECIPIENT_MARKER) == -1 || message.indexOf(URL_MARKER) ==-1 || message.indexOf(FIRSTNAME_MARKER) ==-1 || message.indexOf(LASTNAME_MARKER) ==-1){
+			throw new InvalidMessageFormatException("Invalid format for registration mail");
 		}
 		String recipient = message.substring(RECIPIENT_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER));
 		LOG.debug("Recipient: "+recipient);
 		int messageParsedIndex = RECIPIENT_MARKER.length()+recipient.length()+ELEMENTS_DELIMITER.length(); // holds the index till the message has been parsed.
-		String url = message.substring(messageParsedIndex, message.indexOf(ELEMENTS_DELIMITER, messageParsedIndex));
+		String url = message.substring(messageParsedIndex+URL_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER, messageParsedIndex));
 		LOG.debug("Url: "+url);
 		messageParsedIndex+=URL_MARKER.length()+url.length()+ELEMENTS_DELIMITER.length();
-		
-		
+		String firstName = message.substring(messageParsedIndex+NAME_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER, messageParsedIndex));
+		LOG.debug("First Name: "+firstName);
+		messageParsedIndex+=FIRSTNAME_MARKER.length()+firstName.length()+ELEMENTS_DELIMITER.length();
+		String lastName = message.substring(messageParsedIndex+LASTNAME_MARKER.length());
+		LOG.debug("Last Name: "+lastName);
+		LOG.debug("Sending registration invite mail");
+		emailServices.sendRegistrationInviteMail(url, recipient, firstName, lastName);
 	}
 	
-	private void parseVerificationMail(String message) throws InvalidMessageFormatException{
+	private void parseMailMessage(String message, EmailHeader header) throws NonFatalException{
+		LOG.debug("Mail message: "+message);
 		if(message.indexOf(RECIPIENT_MARKER) == -1 || message.indexOf(URL_MARKER) == -1 || message.indexOf(NAME_MARKER) == -1){
-			throw new InvalidMessageFormatException("Invalid format for verification mail");
+			throw new InvalidMessageFormatException("Invalid format for "+header.getName()+" mail");
 		}
 		String recipient = message.substring(RECIPIENT_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER));
 		LOG.debug("Recipient: "+recipient);
@@ -105,7 +132,90 @@ public class TopicConsumer implements Runnable {
 		messageParsedIndex+=URL_MARKER.length()+url.length()+ELEMENTS_DELIMITER.length();
 		String name = message.substring(messageParsedIndex+NAME_MARKER.length());
 		LOG.debug("Name: "+name);
-		//TODO: Send to the email services
+		if(header == EmailHeader.VERFICATION){
+			LOG.debug("Sending verification mail");
+			emailServices.sendVerificationMail(url, recipient, name);
+		}else if(header == EmailHeader.RESET_PASSWORD){
+			LOG.debug("Sending reset password mail");
+			emailServices.sendResetPasswordEmail(url, recipient, name);
+		}else if(header == EmailHeader.REGISTRATION_COMPLETE){
+			LOG.debug("Sending registration complete mail");
+			emailServices.sendRegistrationCompletionEmail(url, recipient, name);
+		}
 	}
+	
+	private void parseSubscriptionChargeUnsucessfulMail(String message) throws NonFatalException{
+		LOG.debug("Subscription charge unsucessful mail message: "+message);
+		if(message.indexOf(RECIPIENT_MARKER) == -1 || message.indexOf(NAME_MARKER) ==-1 || message.indexOf(RETRYDAYS_MARKER) ==-1){
+			throw new InvalidMessageFormatException("Invalid format for subscription charge unsucessful mail");
+		}
+		String recipient = message.substring(RECIPIENT_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER));
+		LOG.debug("Recipient: "+recipient);
+		int messageParsedIndex = RECIPIENT_MARKER.length()+recipient.length()+ELEMENTS_DELIMITER.length(); // holds the index till the message has been parsed.
+		String name = message.substring(messageParsedIndex+NAME_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER, messageParsedIndex));
+		LOG.debug("Name: "+name);
+		messageParsedIndex+=NAME_MARKER.length()+name.length()+ELEMENTS_DELIMITER.length();
+		String retryDays = message.substring(messageParsedIndex+RETRYDAYS_MARKER.length());
+		LOG.debug("retryDays: "+retryDays);
+		LOG.debug("Sending subscription charge unsucessful mail");
+		emailServices.sendSubscriptionChargeUnsuccessfulEmail(recipient, name, retryDays);
+	}
+	
+	private void parseRetryChargeMail(String message) throws NonFatalException{
+		LOG.debug("Retry charge mail message: "+message);
+		if(message.indexOf(RECIPIENT_MARKER) == -1 || message.indexOf(NAME_MARKER) ==-1 || message.indexOf(RETRIES_MARKER) ==-1){
+			throw new InvalidMessageFormatException("Invalid format for retry charge mail");
+		}
+		String recipient = message.substring(RECIPIENT_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER));
+		LOG.debug("Recipient: "+recipient);
+		int messageParsedIndex = RECIPIENT_MARKER.length()+recipient.length()+ELEMENTS_DELIMITER.length(); // holds the index till the message has been parsed.
+		String name = message.substring(messageParsedIndex+NAME_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER, messageParsedIndex));
+		LOG.debug("Name: "+name);
+		messageParsedIndex+=NAME_MARKER.length()+name.length()+ELEMENTS_DELIMITER.length();
+		String retries = message.substring(messageParsedIndex+RETRIES_MARKER.length());
+		LOG.debug("Retries: "+retries);
+		LOG.debug("Sending retry charge mail");
+		emailServices.sendRetryChargeEmail(recipient, name, retries);
+	}
+	
+	private void parseMailWithRecipientAndName(String message, EmailHeader header) throws NonFatalException{
+		LOG.debug("Message for "+header.getName()+": "+message);
+		if(message.indexOf(RECIPIENT_MARKER) == -1 || message.indexOf(NAME_MARKER) ==-1){
+			throw new InvalidMessageFormatException("Invalid message format for "+header.getName());
+		}
+		String recipient = message.substring(RECIPIENT_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER));
+		LOG.debug("Recipient: "+recipient);
+		int messageParsedIndex = RECIPIENT_MARKER.length()+recipient.length()+ELEMENTS_DELIMITER.length(); // holds the index till the message has been parsed.
+		String name = message.substring(messageParsedIndex+NAME_MARKER.length());
+		LOG.debug("Name: "+name);
+		if(header == EmailHeader.RETRY_EXHAUSTED){
+			LOG.debug("Sending retry exhausted mail");
+			emailServices.sendRetryExhaustedEmail(recipient, name);
+		}else if(header == EmailHeader.ACCOUNT_DISABLED){
+			LOG.debug("Sending account disabled mail");
+			emailServices.sendAccountDisabledMail(recipient, name);
+		}else if(header == EmailHeader.ACCOUNT_UPGRADE){
+			LOG.debug("Sending account upgrade mail");
+			emailServices.sendAccountUpgradeMail(recipient, name);
+		}
+	}
+	
+	private void parseSurveyCompletionMail(String message) throws NonFatalException{
+		LOG.debug("Survey completion mail message: "+message);
+		if(message.indexOf(RECIPIENT_MARKER) == -1 || message.indexOf(NAME_MARKER) ==-1 || message.indexOf(AGENTNAME_MARKER) ==-1){
+			throw new InvalidMessageFormatException("Invalid format for Survey completion mail");
+		}
+		String recipient = message.substring(RECIPIENT_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER));
+		LOG.debug("Recipient: "+recipient);
+		int messageParsedIndex = RECIPIENT_MARKER.length()+recipient.length()+ELEMENTS_DELIMITER.length(); // holds the index till the message has been parsed.
+		String name = message.substring(messageParsedIndex+NAME_MARKER.length(), message.indexOf(ELEMENTS_DELIMITER, messageParsedIndex));
+		LOG.debug("Name: "+name);
+		messageParsedIndex+=NAME_MARKER.length()+name.length()+ELEMENTS_DELIMITER.length();
+		String agentName = message.substring(messageParsedIndex+AGENTNAME_MARKER.length());
+		LOG.debug("Agent name: "+agentName);
+		LOG.debug("Sending account completion mail");
+		emailServices.sendSurveyCompletionMail(recipient, name, agentName);
+	}
+	
 
 }
--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/starter/EmailConsumer.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/starter/EmailConsumer.java
@@ -1,12 +1,21 @@
 package com.realtech.socialsurvey.core.starter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.realtech.socialsurvey.core.services.mq.impl.KafkaConsumerGroup;
 
 public class EmailConsumer {
 
+	public static final Logger LOG = LoggerFactory.getLogger(EmailConsumer.class);
+	
 	public static void main(String[] args){
+		LOG.info("Starting up the email consumer.");
+		LOG.debug("Loading the application context");
+		ApplicationContext context = new ClassPathXmlApplicationContext("resources/sscore-beans.xml");
 		if(args.length != 4){
-			System.out.println("Usage: java EmailConsumer <zookeeper broker list> <groupid> <topic> <number of threads>");
+			LOG.warn("Usage: java EmailConsumer <zookeeper broker list> <groupid> <topic> <number of threads>");
 			System.exit(1);
 		}
 		// Get the parameters
@@ -18,10 +27,10 @@ public class EmailConsumer {
 		try{
 			iNoOfThreads = Integer.parseInt(noOfThreads);
 		}catch(NumberFormatException nfe){
-			System.out.println("Number of threads is not provided properly.");
+			LOG.error("Number of threads is not provided properly.");
 			System.exit(1);
 		}
-		System.out.println("Running the consumer group with zookeeper: "+zookeeper+" groupId: "+ groupId+" topic: "+topic+" num of threads: "+iNoOfThreads);
+		LOG.info("Running the consumer group with zookeeper: "+zookeeper+" groupId: "+ groupId+" topic: "+topic+" num of threads: "+iNoOfThreads);
 		KafkaConsumerGroup consumerGroup = new KafkaConsumerGroup(zookeeper, groupId, topic);
 		consumerGroup.run(iNoOfThreads);
 	}
