a27cd943e3575de3084821610a88e7f2b026cfb6
aswinsegu <aswin.segu@raremile.com>
2015-03-03 19:21:44 +0530

    Minor changes

--- Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/common/JspResolver.java
+++ Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/common/JspResolver.java
@@ -11,16 +11,15 @@ public interface JspResolver {
 	public static final String MESSAGE_HEADER = "messageheader";
 	public static final String LOGIN = "login";
 	public static final String USER_LOGIN = "userlogin";
-	
 	public static final String FORGOT_PASSWORD = "forgotpassword";
 	public static final String RESET_PASSWORD = "resetpassword";
 	public static final String REGISTRATION = "registration";
 	public static final String COMPANY_INFORMATION = "companyinformation";
 	public static final String ACCOUNT_TYPE_SELECTION = "accounttypeselection";
+	public static final String UPGRADE_CONFIRMATION = "upgradeconfirmation";
 	public static final String COMPLETE_REGISTRATION = "completeregistration";
 	public static final String LINKEDIN_ACCESS = "linkedinaccess";
 	public static final String LINKEDIN_MESSAGE = "linkedinmessage";
-
 	public static final String PAYMENT = "payment";
 	public static final String LANDING = "landing";
 	public static final String HIERARCHY_MANAGEMENT = "hierarchymanagement";
@@ -37,7 +36,6 @@ public interface JspResolver {
 	public static final String COMPANY_SETTINGS = "settings";
 	public static final String PAYMENT_ALREADY_MADE = "paymentalreadymade";
 	public static final String CHANGE_PASSWORD = "changepassword";
-	public static final String UPGRADE_CONFIRMATION = "upgradeconfirmation";
 	public static final String INDEX = "index";
 	public static final String PROFILE_LIST = "proList";
 	public static final String PROFILE_EDIT = "profile_edit";
