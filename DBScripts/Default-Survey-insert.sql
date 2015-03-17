ed3603300f6cad4248d0cff2a57d3891ad71c360
Nishit Kannan <niskan@gmail.com>
2015-03-17 18:05:19 +0530

    Changes for verticals

--- DBScripts/Default-Survey-insert.sql
+++ DBScripts/Default-Survey-insert.sql
@@ -27,21 +27,21 @@ UNLOCK TABLES;
 
 LOCK TABLES `SURVEY` WRITE;
 /*!40000 ALTER TABLE `SURVEY` DISABLE KEYS */;
-INSERT INTO `SURVEY` VALUES (2,'Default Banking Survey',1,1,1,'2015-02-10 08:14:34','1','2015-02-10 08:09:53','1'),(3,'Default Realtor Survey',3,1,1,'2015-02-10 08:15:11','1','2015-02-10 08:12:44','1'),(4,'Default Mortgage Survey',2,1,1,'2015-02-10 08:16:41','1','2015-02-10 08:15:17','1');
+INSERT INTO `SURVEY` VALUES (2,'Default Mortgage Survey',1,1,1,'2015-02-10 08:16:41','1','2015-02-10 08:15:17','1');
 /*!40000 ALTER TABLE `SURVEY` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
 LOCK TABLES `SURVEY_QUESTIONS` WRITE;
 /*!40000 ALTER TABLE `SURVEY_QUESTIONS` DISABLE KEYS */;
-INSERT INTO `SURVEY_QUESTIONS` VALUES (1,'sb-range-smiles','Default Banking Question 1',0,'2015-02-10 08:10:17','1','2015-02-10 08:10:17','1'),(2,'sb-range-star','Default Banking Question 1',0,'2015-02-10 08:10:35','1','2015-02-10 08:10:35','1'),(3,'sb-range-scale','Default Banking Question 3',0,'2015-02-10 08:10:48','1','2015-02-10 08:10:48','1'),(4,'sb-range-smiles','Default Realtor Question 1',0,'2015-02-10 08:13:00','1','2015-02-10 08:13:00','1'),(5,'sb-range-star','Default Realtor Question 2',0,'2015-02-10 08:13:15','1','2015-02-10 08:13:15','1'),(6,'sb-range-scale','Default Realtor Question 3',0,'2015-02-10 08:13:28','1','2015-02-10 08:13:28','1'),(7,'sb-range-smiles','Default Mortgage Question 1',0,'2015-02-10 08:15:42','1','2015-02-10 08:15:42','1'),(8,'sb-range-star','Default Mortgage Question 2',0,'2015-02-10 08:15:57','1','2015-02-10 08:15:57','1'),(9,'sb-range-scale','Default Mortgage Question 3',0,'2015-02-10 08:16:05','1','2015-02-10 08:16:05','1');
+INSERT INTO `SURVEY_QUESTIONS` VALUES (1,'sb-range-star','Please rate the level of advice and communication you received from [name]?',1, NOW(),'1',NOW(),'1'),(2,'sb-range-star','How would you rate the service and support you received?',1,NOW(),'1',NOW(),'1'),(3,'sb-range-star','How likely are you to refer friends and family to [name]?',1,NOW(),'1',NOW(),'1');
 /*!40000 ALTER TABLE `SURVEY_QUESTIONS` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
 LOCK TABLES `SURVEY_QUESTIONS_MAPPING` WRITE;
 /*!40000 ALTER TABLE `SURVEY_QUESTIONS_MAPPING` DISABLE KEYS */;
-INSERT INTO `SURVEY_QUESTIONS_MAPPING` VALUES (1,2,1,1,1,1,'2015-02-10 08:10:17','1','2015-02-10 08:10:17','1'),(2,2,2,2,1,1,'2015-02-10 08:10:35','1','2015-02-10 08:10:35','1'),(3,2,3,3,1,1,'2015-02-10 08:10:48','1','2015-02-10 08:10:48','1'),(4,3,4,1,1,1,'2015-02-10 08:13:00','1','2015-02-10 08:13:00','1'),(5,3,5,2,1,1,'2015-02-10 08:13:15','1','2015-02-10 08:13:15','1'),(6,3,6,3,1,1,'2015-02-10 08:13:28','1','2015-02-10 08:13:28','1'),(7,4,7,1,1,1,'2015-02-10 08:15:42','1','2015-02-10 08:15:42','1'),(8,4,8,2,1,1,'2015-02-10 08:15:57','1','2015-02-10 08:15:57','1'),(9,4,9,3,1,1,'2015-02-10 08:16:05','1','2015-02-10 08:16:05','1');
+INSERT INTO `SURVEY_QUESTIONS_MAPPING` VALUES (1,2,1,1,1,1,NOW(),'1',NOW(),'1'),(2,2,2,2,1,1,NOW(),'1',NOW(),'1'),(3,2,3,3,1,1,NOW(),'1',NOW(),'1');
 /*!40000 ALTER TABLE `SURVEY_QUESTIONS_MAPPING` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
