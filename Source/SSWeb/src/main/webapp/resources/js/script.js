28b6f9fbfd2770c84aab3f8640bd6bf3c9a4ce2e
vinod muralidhar <vinod.muralidhar@raremile.com>
2015-03-18 18:27:09 +0530

    Success and Error

--- Source/SSWeb/src/main/webapp/resources/js/script.js
+++ Source/SSWeb/src/main/webapp/resources/js/script.js
@@ -40,13 +40,13 @@ function hideError(){
 function showInfo(msg){
     $('#err-nw-txt').html(msg);
     $('#err-nw-wrapper').slideDown(200);
-    $('#err-nw-wrapper').addClass('bg-black-info');
+    $('#err-nw-wrapper').addClass('bg-black-success');
 }
 
 function hideInfo(){
     $('#err-nw-wrapper').slideUp(200);
     setTimeout(function(){
-        $('#err-nw-wrapper').removeClass('bg-black-info');
+        $('#err-nw-wrapper').removeClass('bg-black-success');
     },200);
 }
 
