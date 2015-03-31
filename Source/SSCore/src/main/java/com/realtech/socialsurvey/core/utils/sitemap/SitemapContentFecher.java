49dc8900c91cda5565d2515138a3cdc9c25fdc53
Nishit Kannan <niskan@gmail.com>
2015-03-31 19:47:59 +0530

    SEO sitemap generator

---
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/utils/sitemap/SitemapContentFecher.java
@@ -0,0 +1,29 @@
+package com.realtech.socialsurvey.core.utils.sitemap;
+
+import java.util.List;
+import com.realtech.socialsurvey.core.entities.SiteMapEntry;
+
+/**
+ * Queries the data store to fetch the content for sitemap
+ *
+ */
+public interface SitemapContentFecher {
+	
+	/**
+	 * Gets the content for sitemap
+	 * @return
+	 */
+	public List<SiteMapEntry> getInitialContent();
+	
+	/**
+	 * Set the flag to check if there is more data
+	 * @return
+	 */
+	public boolean hasNext();
+	
+	/**
+	 * Gets the next set of records
+	 * @return
+	 */
+	public List<SiteMapEntry> nextBatch();
+}
