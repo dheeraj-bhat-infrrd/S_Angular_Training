14710f3cc04ed3fcfe45ba9e1b5893daa959259d
aswinsegu <aswin.segu@raremile.com>
2015-04-11 11:55:01 +0530

    SS-305 Fix for url validation

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/utils/UrlValidationHelper.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/utils/UrlValidationHelper.java
@@ -14,11 +14,35 @@ public class UrlValidationHelper {
 
 	public void validateUrl(String url) throws IOException {
 		LOG.info("Checking if url exists : {}", url);
-		URL u = new URL(url);
-		HttpURLConnection huc = (HttpURLConnection) u.openConnection();
-		huc.setRequestMethod("GET"); // OR huc.setRequestMethod ("HEAD");
-		huc.connect();
+		
+		// checking for https protocol
+		String tempUrl = new String(url);
+		if (!tempUrl.startsWith("https://")) {
+			tempUrl = "https://" + tempUrl;
+	    }
+		
+		URL connectUrl = null;
+		HttpURLConnection huc;
+		try {
+			connectUrl = new URL(tempUrl);
+			huc = (HttpURLConnection) connectUrl.openConnection();
+			huc.setRequestMethod("GET"); // OR huc.setRequestMethod ("HEAD");
+			huc.connect();
+		}
+		catch (IOException e) {
+			LOG.info("Url-{} does not support https", url);
+			
+			// checking for http protocol
+			tempUrl = new String(url);
+			if (!tempUrl.startsWith("http://")) {
+				tempUrl = "http://" + tempUrl;
+		    }
+
+			connectUrl = new URL(tempUrl);
+			huc = (HttpURLConnection) connectUrl.openConnection();
+			huc.setRequestMethod("GET"); // OR huc.setRequestMethod ("HEAD");
+			huc.connect();
+		}
 		LOG.info("Url-{} validated successfully", url);
 	}
-
 }
--- Source/SSWeb/src/main/webapp/resources/js/editprofile.js
+++ Source/SSWeb/src/main/webapp/resources/js/editprofile.js
@@ -4,7 +4,7 @@ var numOfRows = 3;
 var minScore = 0;
 var attrName = null;
 var attrVal = null;
-var webAddressRegEx = /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+var webAddressRegEx = /((http(s?):\/\/)?)(www.)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w.?=&_]+)?/;
 var timer = 0;
 var profileId;
 var delay = (function() {
@@ -950,7 +950,7 @@ function callBackUpdateYelpLink(data) {
 
 function isValidUrl(url){
 	var myVariable = url;
-	if(/^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test(myVariable)) {
+	if(webAddressRegEx.test(myVariable)) {
 		return true;
 	} else {
 		return false;
