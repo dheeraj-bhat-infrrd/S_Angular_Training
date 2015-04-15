a96ec337c6bda614c64f3ee6dec87712e260db7a
Nishit Kannan <niskan@gmail.com>
2015-04-15 20:12:20 +0530

    Fetching profile data from LinkedIn

---
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/Country.java
@@ -0,0 +1,14 @@
+package com.realtech.socialsurvey.core.entities;
+
+public class Country {
+
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+}
