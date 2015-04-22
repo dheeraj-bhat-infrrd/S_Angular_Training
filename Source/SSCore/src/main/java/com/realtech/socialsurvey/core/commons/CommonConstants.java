5a1351da8d70d15535ce8f8454770ec94d42140f
ritwik <ritwik@raremile.com>
2015-04-22 20:05:14 +0530

    To store default text for survey invitation and reminder mail.

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/commons/CommonConstants.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/commons/CommonConstants.java
@@ -257,6 +257,8 @@ public interface CommonConstants {
 	public static final String BRANCH_PROFILE_FIXED_URL = "pages/branch/";
 	public static final String REGION_PROFILE_FIXED_URL = "pages/region/";
 	public static final String COMPANY_PROFILE_FIXED_URL = "pages/company/";
+	public static final String SURVEY_REQUEST_MAIL_FILENAME = "EmailTemplates/SurveyParticipationMailBody.html";
+	public static final String SURVEY_REMINDER_MAIL_FILENAME = "EmailTemplates/SurveyParticipationMailBody.html";
 	
 	/**
 	 * Logo related config
