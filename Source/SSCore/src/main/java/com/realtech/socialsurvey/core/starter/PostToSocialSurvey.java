bd319873f231c23f03c9e74e274dba089b1bbad3
aswinsegu <aswin.segu@raremile.com>
2015-04-23 16:00:35 +0530

    Minor changes for Batch PostToWall

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/starter/PostToSocialSurvey.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/starter/PostToSocialSurvey.java
@@ -1,10 +1,13 @@
 package com.realtech.socialsurvey.core.starter;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.realtech.socialsurvey.core.utils.mongo.PostToWall;
 
 public class PostToSocialSurvey {
 
@@ -12,18 +15,14 @@ public class PostToSocialSurvey {
 
 	public static void main(String[] args) {
 		LOG.info("Starting up the PostToSocialSurvey");
+
+		// TODO parse emailIds
+		List<String> emailIds = new ArrayList<String>();
+		emailIds.add("ss_agent1@mailinator.com");
+
 		ApplicationContext context = new ClassPathXmlApplicationContext("ss-starter-config.xml");
-
-		Runnable postToWall = (Runnable) context.getBean("postToWall");
-		Thread postToWallThread = new Thread(postToWall);
-		postToWallThread.start();
-
-		try {
-			postToWallThread.join();
-		}
-		catch (InterruptedException e) {
-			LOG.error("Exception while joining th import threads. ", e);
-		}
+		PostToWall postToWall = (PostToWall) context.getBean("postToWall");
+		postToWall.postStatusForUser(emailIds);
 
 		// Closing the context
 		LOG.info("Finished the PostToSocialSurvey");
