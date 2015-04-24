5281b5cb2aae1dbccb84b5664ea370e37005bf95
aswinsegu <aswin.segu@raremile.com>
2015-04-24 15:51:59 +0530

    Removed unwanted jsps

--- Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/common/JspResolver.java
+++ Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/common/JspResolver.java
@@ -4,48 +4,81 @@ package com.realtech.socialsurvey.web.common;
  * Holds the jsp name as constants
  */
 public interface JspResolver {
-	public static final String CORPORATE_INVITATION = "corporateinvitation";
-	public static final String INVITATION = "invitation";
-	public static final String ERROR_PAGE = "errorpage500";
-	public static final String MESSAGE_HEADER = "messageheader";
-	public static final String LOGIN = "login";
-	public static final String USER_LOGIN = "userlogin";
-	public static final String FORGOT_PASSWORD = "forgotpassword";
-	public static final String RESET_PASSWORD = "resetpassword";
+	public static final String REGIONS_AUTOCOMPLETE = "regionsautocomplete";
+	public static final String USER_ID_ON_INVITE = "useridoninvite";
+
+	// Registration
 	public static final String REGISTRATION = "registration";
 	public static final String COMPANY_INFORMATION = "companyinformation";
 	public static final String ACCOUNT_TYPE_SELECTION = "accounttypeselection";
-	public static final String UPGRADE_CONFIRMATION = "upgradeconfirmation";
 	public static final String COMPLETE_REGISTRATION = "completeregistration";
+	public static final String LINKEDIN_ACCESS = "linkedinaccess";
+	public static final String INVITATION = "invitation";
+
+	// Generic pages
+	public static final String INDEX = "index";
+	public static final String MESSAGE_HEADER = "messageheader";
+	public static final String ERROR_PAGE = "errorpage500";
+	public static final String LOGIN = "login";
+	public static final String PROFILE_LIST = "proList";
+	public static final String PROFILE_PAGE = "profile";
+
+	// Password handling
+	public static final String CHANGE_PASSWORD = "changepassword";
+	public static final String FORGOT_PASSWORD = "forgotpassword";
+	public static final String RESET_PASSWORD = "resetpassword";
+
+	// Payment handling
 	public static final String PAYMENT = "payment";
+	public static final String PAYMENT_ALREADY_MADE = "paymentalreadymade";
+	public static final String UPGRADE_CONFIRMATION = "upgradeconfirmation";
+
+	// Dashboard
+	public static final String USER_LOGIN = "userlogin";
 	public static final String LANDING = "landing";
-	public static final String HIERARCHY_MANAGEMENT = "hierarchymanagement";
-	public static final String SURVEY_BUILDER = "buildSurvey";
-	public static final String SURVEY_BUILDER_QUESTION_OVERLAY = "buildSurvey_questionoverlay";
-	public static final String SURVEY_BUILDER_QUESTION_LIST = "buildSurvey_questionlist";
-	public static final String SURVEY_BUILDER_QUESTION_EDIT = "buildSurvey_questionedit";
-	public static final String SURVEY_BUILDER_QUESTION_NEW = "buildSurvey_questionnew";
 	public static final String DASHBOARD = "dashboard";
 	public static final String DASHBOARD_PROFILEDETAIL = "dashboard_profiledetail";
 	public static final String DASHBOARD_SURVEYSTATUS = "dashboard_surveystatus";
 	public static final String DASHBOARD_SEARCHRESULTS = "dashboard_searchresults";
 	public static final String DASHBOARD_INCOMPLETESURVEYS = "dashboard_incompletesurveys";
 	public static final String DASHBOARD_REVIEWS = "dashboard_reviews";
+
+	// Hierarchy Management
+	public static final String BUILD_HIERARCHY = "build-hierarchy";
+	public static final String HIERARCHY_REGION_EDIT = "hierarchy-region-edit";
+	public static final String HIERARCHY_OFFICE_EDIT = "hierarchy-office-edit";
+	public static final String HIERARCHY_INDIVIDUAL_EDIT = "hierarchy-individual-edit";
+	
+	public static final String VIEW_HIERARCHY = "hierarchy-view";
+	public static final String VIEW_HIERARCHY_REGION_LIST = "hierarchy-view-regions-list";
+	public static final String VIEW_HIERARCHY_BRANCH_LIST = "hierarchy-view-branches-list";
+	public static final String VIEW_HIERARCHY_USERS_LIST = "hierarchy-view-users-list";
+
+	public static final String EXISTING_BRANCHES = "existingbranches";
+	public static final String EXISTING_ENTERPRISE_REGIONS = "existingenterpriseregions";
+	public static final String EXISTING_ENTERPRISE_BRANCHES = "existingenterprisebranches";
+
+	// Survey Management
+	public static final String SURVEY_BUILDER = "buildSurvey";
+	public static final String SURVEY_BUILDER_QUESTION_OVERLAY = "buildSurvey_questionoverlay";
+	public static final String SURVEY_BUILDER_QUESTION_LIST = "buildSurvey_questionlist";
+	public static final String SURVEY_BUILDER_QUESTION_EDIT = "buildSurvey_questionedit";
+	public static final String SURVEY_BUILDER_QUESTION_NEW = "buildSurvey_questionnew";
+	public static final String SHOW_SURVEY_QUESTIONS = "surveyQuestion";
+	public static final String SURVEY_REQUEST = "surveyRequest";
+
+	// User Management
 	public static final String USER_MANAGEMENT = "view-user-management";
 	public static final String USER_LIST_FOR_MANAGEMENT = "userlist";
 	public static final String USER_MANAGEMENT_EDIT_USER_DETAILS = "um-edit-row";
 	public static final String USER_DETAILS = "userdetails";
 	public static final String USER_LIST = "userslist";
-	public static final String USER_ID_ON_INVITE = "useridoninvite";
-	public static final String EXISTING_BRANCHES = "existingbranches";
-	public static final String EXISTING_ENTERPRISE_REGIONS = "existingenterpriseregions";
-	public static final String EXISTING_ENTERPRISE_BRANCHES = "existingenterprisebranches";
-	public static final String REGIONS_AUTOCOMPLETE = "regionsautocomplete";
+
+	// Edit Settings
 	public static final String EDIT_SETTINGS = "settings";
-	public static final String PAYMENT_ALREADY_MADE = "paymentalreadymade";
-	public static final String CHANGE_PASSWORD = "changepassword";
-	public static final String INDEX = "index";
-	public static final String PROFILE_LIST = "proList";
+	public static final String SOCIAL_AUTH_MESSAGE = "socialauthmessage";
+
+	// Profile Settings
 	public static final String PROFILE_EDIT = "profile_edit";
 	public static final String PROFILE_CONTACT_DETAILS = "profile_contactdetails";
 	public static final String PROFILE_ABOUT_ME = "profile_aboutme";
@@ -59,17 +92,4 @@ public interface JspResolver {
 	public static final String PROFILE_HIERARCHY = "profile_hierarchy";
 	public static final String PROFILE_HIERARCHY_CLICK_REGION = "profile_hierarchy_region";
 	public static final String PROFILE_HIERARCHY_CLICK_BRANCH = "profile_hierarchy_branch";
-	public static final String PROFILE_PAGE = "profile";
-	public static final String BUILD_HIERARCHY = "build-hierarchy";
-	public static final String VIEW_HIERARCHY = "hierarchy-view";
-	public static final String VIEW_HIERARCHY_REGION_LIST = "hierarchy-view-regions-list";
-	public static final String VIEW_HIERARCHY_BRANCH_LIST = "hierarchy-view-branches-list";
-	public static final String HIERARCHY_REGION_EDIT = "hierarchy-region-edit";
-	public static final String HIERARCHY_OFFICE_EDIT = "hierarchy-office-edit";
-	public static final String HIERARCHY_INDIVIDUAL_EDIT = "hierarchy-individual-edit";
-	public static final String LINKEDIN_ACCESS = "linkedinaccess";
-	public static final String SOCIAL_AUTH_MESSAGE = "socialauthmessage";
-	public static final String VIEW_HIERARCHY_USERS_LIST = "hierarchy-view-users-list";
-	public static final String SHOW_SURVEY_QUESTIONS = "surveyQuestion";
-	public static final String SURVEY_REQUEST = "surveyRequest";
 }
