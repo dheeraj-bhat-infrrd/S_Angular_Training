2b6d6495c99c2ee4458e37e69ef21e1b0067b3ea
shubham agrawal <shubham@raremile.com>
2015-05-07 15:07:50 +0530

    1. Bug fixes and style changes for home page, sign up path. 2. Added opensans fonts for IE browser. 3. Added DB script for state and zipcode lookup.

---
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/StateLookup.java
@@ -0,0 +1,71 @@
+package com.realtech.socialsurvey.core.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * The persistent class for the statelookup database table.
+ * 
+ */
+@Entity
+@Table(name = "statelookup")
+@NamedQuery(name = "StateLookup.findAll", query = "SELECT s FROM StateLookup s")
+public class StateLookup implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String statecode;
+	private String statename;
+	private List<ZipCodeLookup> codeLookups;
+
+	public StateLookup() {
+	}
+
+	public StateLookup(Integer id) {
+		this.id = id;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStatecode() {
+		return statecode;
+	}
+
+	public void setStatecode(String statecode) {
+		this.statecode = statecode;
+	}
+
+	public String getStatename() {
+		return statename;
+	}
+
+	public void setStatename(String statename) {
+		this.statename = statename;
+	}
+
+	@OneToMany(mappedBy = "stateLookup", fetch = FetchType.LAZY)
+	public List<ZipCodeLookup> getCodeLookups() {
+		return this.codeLookups;
+	}
+
+	public void setCodeLookups(List<ZipCodeLookup> codeLookups) {
+		this.codeLookups = codeLookups;
+	}
+
+}
