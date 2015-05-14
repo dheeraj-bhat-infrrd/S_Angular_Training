7b7f3664de377ad2fdca0d512d355ab35f9c3d5d
aswinsegu <aswin.segu@raremile.com>
2015-05-14 22:08:27 +0530

    Changes in google auth

---
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/GoogleToken.java
@@ -0,0 +1,64 @@
+package com.realtech.socialsurvey.core.entities;
+
+public class GoogleToken {
+
+	private String googleId;
+	private String profileLink;
+	private String googleAccessToken;
+	private String googleRefreshToken;
+	private long googleAccessTokenCreatedOn;
+	private long googleAccessTokenExpiresIn;
+
+	public String getGoogleId() {
+		return googleId;
+	}
+
+	public void setGoogleId(String googleId) {
+		this.googleId = googleId;
+	}
+
+	public String getProfileLink() {
+		return profileLink;
+	}
+
+	public void setProfileLink(String profileLink) {
+		this.profileLink = profileLink;
+	}
+
+	public String getGoogleAccessToken() {
+		return googleAccessToken;
+	}
+
+	public void setGoogleAccessToken(String googleAccessToken) {
+		this.googleAccessToken = googleAccessToken;
+	}
+
+	public String getGoogleRefreshToken() {
+		return googleRefreshToken;
+	}
+
+	public void setGoogleRefreshToken(String googleRefreshToken) {
+		this.googleRefreshToken = googleRefreshToken;
+	}
+
+	public long getGoogleAccessTokenCreatedOn() {
+		return googleAccessTokenCreatedOn;
+	}
+
+	public void setGoogleAccessTokenCreatedOn(long googleAccessTokenCreatedOn) {
+		this.googleAccessTokenCreatedOn = googleAccessTokenCreatedOn;
+	}
+
+	public long getGoogleAccessTokenExpiresIn() {
+		return googleAccessTokenExpiresIn;
+	}
+
+	public void setGoogleAccessTokenExpiresIn(long googleAccessTokenExpiresIn) {
+		this.googleAccessTokenExpiresIn = googleAccessTokenExpiresIn;
+	}
+
+	@Override
+	public String toString() {
+		return "FacebookToken [googleId=" + googleId + ", googlePageLink=" + profileLink + "]";
+	}
+}
