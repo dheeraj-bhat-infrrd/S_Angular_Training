a5ce168bab1d6263f9be81bced3410d3cd8583ca
ritwik <ritwik@raremile.com>
2015-05-21 19:43:39 +0530

    Formatting

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/search/impl/SolrSearchServiceImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/search/impl/SolrSearchServiceImpl.java
@@ -692,18 +692,18 @@ public class SolrSearchServiceImpl implements SolrSearchService {
 		solrQuery.setQuery(CommonConstants.USER_ID_SOLR + ":" + userId);
 
 		LOG.debug("Querying solr for searching users");
-		try{
-		response = solrServer.query(solrQuery);
-		SolrDocumentList results = response.getResults();
+		try {
+			response = solrServer.query(solrQuery);
+			SolrDocumentList results = response.getResults();
 
-		if (results != null && !results.isEmpty()) {
-			solrDocument = results.get(CommonConstants.INITIAL_INDEX);
+			if (results != null && !results.isEmpty()) {
+				solrDocument = results.get(CommonConstants.INITIAL_INDEX);
+			}
+			else {
+				LOG.debug("No user present in solr for the userId:" + userId);
+			}
 		}
-		else {
-			LOG.debug("No user present in solr for the userId:" + userId);
-		}
-		}
-		catch(SolrServerException e){
+		catch (SolrServerException e) {
 			throw new SolrException("SolrServerException caught in getUserByUniqueId().", e);
 		}
 		LOG.info("Method getUserByUniqueId executed succesfully. Returning :" + solrDocument);
@@ -889,7 +889,7 @@ public class SolrSearchServiceImpl implements SolrSearchService {
 				default:
 					solrServer = new HttpSolrServer(solrRegionUrl);
 			}
-			
+
 			query.setQuery(columnName + ":" + id);
 
 			query.addFilterQuery(searchColumn + ":" + searchKey);
@@ -1203,29 +1203,31 @@ public class SolrSearchServiceImpl implements SolrSearchService {
 		}
 		LOG.info("Method to increase completed survey count updateCompletedSurveyCountForUserInSolr() finished.");
 	}
-	
+
 	@Override
-	public Map<String, String> getCompanyAdmin(long companyId) throws SolrException{
+	public Map<String, String> getCompanyAdmin(long companyId) throws SolrException {
 		LOG.info("Method getEmailIdOfCompanyAdmin() started");
-		try{SolrServer solrServer;
-		solrServer = new HttpSolrServer(solrUserUrl);
-		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery(CommonConstants.COMPANY_ID_SOLR + ":" + companyId);
-		solrQuery.addFilterQuery("isOwner" + ":" + "1");
-		QueryResponse response = solrServer.query(solrQuery);
-		SolrDocumentList results = response.getResults();
-		LOG.info("Method getEmailIdOfCompanyAdmin() finished");
-		if (results != null && !results.isEmpty()) {
-			SolrDocument solrDocument = results.get(CommonConstants.INITIAL_INDEX);
-			if(solrDocument != null){
-				Map<String, String> companyAdmin = new HashMap<>();
-				companyAdmin.put("displayName", (String)solrDocument.get("displayName"));
-				companyAdmin.put("loginName", (String)solrDocument.get("loginName"));
-				companyAdmin.put("emailId", (String)solrDocument.get("emailId"));
-				return companyAdmin;
+		try {
+			SolrServer solrServer;
+			solrServer = new HttpSolrServer(solrUserUrl);
+			SolrQuery solrQuery = new SolrQuery();
+			solrQuery.setQuery(CommonConstants.COMPANY_ID_SOLR + ":" + companyId);
+			solrQuery.addFilterQuery("isOwner" + ":" + "1");
+			QueryResponse response = solrServer.query(solrQuery);
+			SolrDocumentList results = response.getResults();
+			LOG.info("Method getEmailIdOfCompanyAdmin() finished");
+			if (results != null && !results.isEmpty()) {
+				SolrDocument solrDocument = results.get(CommonConstants.INITIAL_INDEX);
+				if (solrDocument != null) {
+					Map<String, String> companyAdmin = new HashMap<>();
+					companyAdmin.put("displayName", (String) solrDocument.get("displayName"));
+					companyAdmin.put("loginName", (String) solrDocument.get("loginName"));
+					companyAdmin.put("emailId", (String) solrDocument.get("emailId"));
+					return companyAdmin;
+				}
 			}
 		}
-		}catch(SolrServerException e){
+		catch (SolrServerException e) {
 			LOG.error("SolrServerException caught in getEmailIdOfCompanyAdmin(). Nested exception is ", e);
 			throw new SolrException("SolrServerException caught in getEmailIdOfCompanyAdmin(). Nested exception is", e);
 		}
