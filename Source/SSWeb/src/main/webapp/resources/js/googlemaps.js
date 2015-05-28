caf118623571ba8df4cf624c6f8dee71a9a2801a
shubham agrawal <shubham@raremile.com>
2015-05-28 14:31:25 +0530

    Bug fix for map.

--- Source/SSWeb/src/main/webapp/resources/js/googlemaps.js
+++ Source/SSWeb/src/main/webapp/resources/js/googlemaps.js
@@ -8,17 +8,21 @@ var apikey;
 $(document).ready(function() {
 	
 	if (typeof apikey === 'undefined') {
-		$.ajax({
-			url : window.location.origin + "/fetchgooglemapapikey.do",
-			type : "GET",
-			dataType : "html",
-			async : true,
-			success : function(data) {
-				apikey = data;
-			},
-			error : function(e) {
-				redirectErrorpage();
-			}
-		});
+		fetchGoogleMapApi();
 	}
 });
+
+function fetchGoogleMapApi() {
+	$.ajax({
+		url : window.location.origin + "/fetchgooglemapapikey.do",
+		type : "GET",
+		dataType : "html",
+		async : false,
+		success : function(data) {
+			apikey = data;
+		},
+		error : function(e) {
+			redirectErrorpage();
+		}
+	});
+}
