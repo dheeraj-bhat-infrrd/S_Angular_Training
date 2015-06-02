ae919100dacf9f5c69f74813e2d967f33d78f8d5
ritwik <ritwik@raremile.com>
2015-06-02 17:11:24 +0530

    Null check for survey preinitiation entry if survey is being retaken

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/surveybuilder/impl/SurveyHandlerImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/surveybuilder/impl/SurveyHandlerImpl.java
@@ -571,7 +571,6 @@ public class SurveyHandlerImpl implements SurveyHandler, InitializingBean {
 	@Transactional
 	public void deleteSurveyPreInitiationDetailsPermanently(SurveyPreInitiation surveyPreInitiation) {
 		LOG.info("Method deleteSurveyPreInitiationDetailsPermanently() started.");
-		surveyPreInitiation = surveyPreInitiationDao.findById(SurveyPreInitiation.class, surveyPreInitiation.getSurveyPreIntitiationId());
 		if(surveyPreInitiation != null)
 			surveyPreInitiationDao.delete(surveyPreInitiation);
 		LOG.info("Method deleteSurveyPreInitiationDetailsPermanently() finished.");
