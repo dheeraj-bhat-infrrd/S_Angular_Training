a2f548304ee9af49690c9246d2aa2d2c45a1b592
utsav09211 <utsav.sharma@raremile.com>
2015-06-29 16:11:47 +0530

    Status Code Changes to 205 For Redirect

--- Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/security/AjaxAwareLoginUrlAuthenticationEntryPoint.java
+++ Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/security/AjaxAwareLoginUrlAuthenticationEntryPoint.java
@@ -1,22 +1,31 @@
 package com.realtech.socialsurvey.web.security;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.ELRequestMatcher;
 import org.springframework.security.web.util.RequestMatcher;
+
+import com.realtech.socialsurvey.core.commons.CommonConstants;
 import com.realtech.socialsurvey.core.exception.UserSessionInvalidateException;
 
 @SuppressWarnings("deprecation")
-public class AjaxAwareLoginUrlAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint {
+public class AjaxAwareLoginUrlAuthenticationEntryPoint extends
+		LoginUrlAuthenticationEntryPoint {
 
-	private static final Logger LOG = LoggerFactory.getLogger(AjaxAwareLoginUrlAuthenticationEntryPoint.class);
-	private static final RequestMatcher requestMatcher = new ELRequestMatcher("hasHeader('X-Requested-With','XMLHttpRequest')");
+	private static final Logger LOG = LoggerFactory
+			.getLogger(AjaxAwareLoginUrlAuthenticationEntryPoint.class);
+	private static final RequestMatcher requestMatcher = new ELRequestMatcher(
+			"hasHeader('X-Requested-With','XMLHttpRequest')");
 
 	public AjaxAwareLoginUrlAuthenticationEntryPoint() {
 		super();
@@ -27,23 +36,39 @@ public class AjaxAwareLoginUrlAuthenticationEntryPoint extends LoginUrlAuthentic
 	}
 
 	@Override
-	public void commence(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException)
-			throws IOException, ServletException {
+	public void commence(final HttpServletRequest request,
+			final HttpServletResponse response,
+			final AuthenticationException authException) throws IOException,
+			ServletException {
 
 		if (requestMatcher.matches(request)) {
 			LOG.info("Redirecting to login.do");
-//			StringBuilder urlBuilder = new StringBuilder();
-//			urlBuilder.append(request.getScheme()).append("://").append(request.getServerName());
-//			// if port is other than 80 or 443, then append that to the url
-//			if(request.getServerPort() != 80 && request.getServerPort() != 443){
-//				urlBuilder.append(":").append(request.getServerPort());
-//			}
-//			RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-//			redirectStrategy.sendRedirect(request, response, urlBuilder.toString());
-//			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-			throw new UserSessionInvalidateException("User session is no longer available.");
-		}
-		else {
+			StringBuilder urlBuilder = new StringBuilder();
+			urlBuilder.append(request.getScheme()).append("://")
+					.append(request.getServerName());
+
+			// if port is other than 80 or 443, then append that to the url
+			if (request.getServerPort() != 80 && request.getServerPort() != 443) {
+				urlBuilder.append(":").append(request.getServerPort());
+			}
+			urlBuilder.append("/login.do?s=sessionerror");
+			LOG.info("Url it is redirecting too " + urlBuilder.toString());
+			/*
+			 * RedirectStrategy redirectStrategy = new
+			 * DefaultRedirectStrategy(); redirectStrategy.sendRedirect(request,
+			 * response, urlBuilder.toString());
+			 */
+			response.setStatus(HttpServletResponse.SC_RESET_CONTENT);
+			/*
+			 * response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+			 * "Unauthorized");
+			 */
+			/*
+			 * throw new UserSessionInvalidateException(
+			 * "User session is no longer available.");
+			 */
+			return;
+		} else {
 			super.commence(request, response, authException);
 		}
 	}
