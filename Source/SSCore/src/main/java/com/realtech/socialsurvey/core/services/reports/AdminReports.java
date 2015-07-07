35ea4884a34f184852e6a01406bdc1ef8ad4fdf8
Nishit Kannan <niskan@gmail.com>
2015-07-07 15:13:16 +0530

    Admin reports

---
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/reports/AdminReports.java
@@ -0,0 +1,24 @@
+package com.realtech.socialsurvey.core.services.reports;
+
+import java.util.List;
+import com.realtech.socialsurvey.core.entities.Company;
+import com.realtech.socialsurvey.core.entities.CompanyReportsSearch;
+import com.realtech.socialsurvey.core.exception.InvalidInputException;
+import com.realtech.socialsurvey.core.exception.NoRecordsFetchedException;
+
+
+/**
+ * Generates reports from the admin panel
+ *
+ */
+public interface AdminReports {
+
+	/**
+	 * Gets company related data
+	 * @param search
+	 * @return
+	 * @throws InvalidInputException
+	 * @throws NoRecordsFetchedException
+	 */
+	public List<Company> companyCreationReports(CompanyReportsSearch search) throws InvalidInputException, NoRecordsFetchedException;
+}
