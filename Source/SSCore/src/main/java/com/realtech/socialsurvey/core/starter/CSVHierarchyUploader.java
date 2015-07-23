b691932e993c9e5a183d36503cfc8f75d71670c1
Nishit Kannan <niskan@gmail.com>
2015-07-23 13:11:29 +0530

    CSV Upload for hierarchy

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/starter/CSVHierarchyUploader.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/starter/CSVHierarchyUploader.java
@@ -6,33 +6,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
-import com.realtech.socialsurvey.core.handler.EmailHandler;
-import com.realtech.socialsurvey.core.services.upload.CsvUploadService;
+import com.realtech.socialsurvey.core.handler.CSVUploadHandler;
 
 /**
  * Uploads the hierarchy
- *
  */
 @Component
-public class CSVHierarchyUploader extends QuartzJobBean{
+public class CSVHierarchyUploader extends QuartzJobBean {
 
-	public static final Logger LOG = LoggerFactory.getLogger( CSVHierarchyUploader.class );
-	
-	private CsvUploadService uploadService;
-	
+	public static final Logger LOG = LoggerFactory.getLogger(CSVHierarchyUploader.class);
+
+	private CSVUploadHandler uploadHandler;
+
 	@Override
-    protected void executeInternal( JobExecutionContext jobExecutionContext )
-    {
-        LOG.info( "ExecutingUpdateSubscriptionPriceStarter " );
-        initializeDependencies( jobExecutionContext.getMergedJobDataMap() );
-        handler.startEmailProcessing();
+	protected void executeInternal(JobExecutionContext jobExecutionContext) {
+		LOG.info("Executing CSVHierarchyUploader");
+		initializeDependencies(jobExecutionContext.getMergedJobDataMap());
+		uploadHandler.startFileUpload();
 
-    }
+	}
 
+	private void initializeDependencies(JobDataMap jobMap) {
 
-    private void initializeDependencies( JobDataMap jobMap )
-    {
-
-    	uploadService = (EmailHandler) jobMap.get( "uploadService" );
-    }
+		uploadHandler = (CSVUploadHandler) jobMap.get("uploadHandler");
+	}
 }
--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/starter/CSVUploadProcessor.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/starter/CSVUploadProcessor.java
@@ -1,11 +1,61 @@
 package com.realtech.socialsurvey.core.starter;
 
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.realtech.socialsurvey.core.commons.CommonConstants;
+import com.realtech.socialsurvey.core.entities.FileUpload;
+import com.realtech.socialsurvey.core.exception.InvalidInputException;
+import com.realtech.socialsurvey.core.exception.NoRecordsFetchedException;
+import com.realtech.socialsurvey.core.services.upload.CsvUploadService;
+
+@Component
 public class CSVUploadProcessor implements Runnable {
 
+	public static final Logger LOG = LoggerFactory.getLogger( CSVUploadProcessor.class );
+	
+	@Autowired
+	private CsvUploadService csvUploadService;
+	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		LOG.info("Checking if any hierarchy needs to be uploaded");
+		while(true){
+			LOG.debug("Checking for any new file upload");
+			// check if there are any files to be uploaded
+			try {
+				List<FileUpload> filesToBeUploaded = csvUploadService.getFilesToBeUploaded();
+				for(FileUpload fileUpload : filesToBeUploaded){
+					try {
+						// update the status to be processing
+						fileUpload.setStatus(CommonConstants.STATUS_UNDER_PROCESSING);
+						csvUploadService.updateFileUploadRecord(fileUpload);
+						// parse the csv
+						csvUploadService.parseAndUploadTempCsv(fileUpload);
+						// update the status to be processed
+						fileUpload.setStatus(CommonConstants.STATUS_INACTIVE);
+						csvUploadService.updateFileUploadRecord(fileUpload);
+					}
+					catch (InvalidInputException e) {
+						LOG.debug("Error updating the status");
+						continue;
+					}
+				}
+			}
+			catch (NoRecordsFetchedException e) {
+				LOG.debug("No files to be uploaded. Sleep for a minute");
+				try {
+					Thread.sleep(1000 * 60);
+				}
+				catch (InterruptedException e1) {
+					LOG.warn("Thread interrupted");
+					break;
+				}
+			}
+			
+		}
 	}
 
 }
