f260e37a3598c7ba2ffd0dde5435847f8224238b
aswinsegu <aswin.segu@raremile.com>
2015-07-31 14:45:22 +0530

    Changes for exisitng session

--- Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/controller/RegistrationController.java
+++ Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/controller/RegistrationController.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -163,8 +164,10 @@ public class RegistrationController
         // Check for existing session
         if ( sessionHelper.isUserActiveSessionExists() ) {
             LOG.info( "Existing Active Session detected" );
-            redirectAttributes.addFlashAttribute( CommonConstants.ACTIVE_SESSIONS_FOUND, "true" );
-            return "redirect:/" + JspResolver.USER_LOGIN + ".do";
+            
+            // Invalidate session in browser
+         	request.getSession(false).invalidate();
+         	SecurityContextHolder.clearContext();
         }
 
         try {
