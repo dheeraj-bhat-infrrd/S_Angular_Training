caf606f6676b067786863e31b3cb40aed1d700f2
aswinsegu <aswin.segu@raremile.com>
2015-08-11 15:08:05 +0530

    Minor changes

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/organizationmanagement/impl/DashboardServiceImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/organizationmanagement/impl/DashboardServiceImpl.java
@@ -220,14 +220,11 @@ public class DashboardServiceImpl implements DashboardService, InitializingBean 
 		CellStyle style = workbook.createCellStyle();
 		style.setDataFormat(df.getFormat("d-mm-yyyy"));
 		Integer counter = 1;
-		int max = 0;
-		int internalMax = 0;
 
 		// This data needs to be written (List<Object>)
 		Map<String, List<Object>> data = new TreeMap<>();
 		List<Object> surveyDetailsToPopulate = new ArrayList<>();
 		for (SurveyPreInitiation survey : surveyDetails) {
-			internalMax = 0;
 			surveyDetailsToPopulate.add(survey.getCustomerFirstName());
 			surveyDetailsToPopulate.add(survey.getCustomerLastName());
 			surveyDetailsToPopulate.add(survey.getCustomerEmailId());
@@ -242,19 +239,14 @@ public class DashboardServiceImpl implements DashboardService, InitializingBean 
 			}
 			data.put((++counter).toString(), surveyDetailsToPopulate);
 			surveyDetailsToPopulate = new ArrayList<>();
-			if (internalMax > max)
-				max = internalMax;
 		}
+		
 		surveyDetailsToPopulate.add("First Name");
 		surveyDetailsToPopulate.add("Last Name");
 		surveyDetailsToPopulate.add("Email Id");
 		surveyDetailsToPopulate.add("Started On");
 		surveyDetailsToPopulate.add("Last Updated On");
 		surveyDetailsToPopulate.add("Link To Survey");
-		for (counter = 1; counter <= max; counter++) {
-			internalMax++;
-			surveyDetailsToPopulate.add("Question " + counter);
-		}
 
 		data.put("1", surveyDetailsToPopulate);
 
@@ -365,18 +357,23 @@ public class DashboardServiceImpl implements DashboardService, InitializingBean 
 		CellStyle style = workbook.createCellStyle();
 		style.setDataFormat(df.getFormat("d-mm-yyyy"));
 		Integer counter = 1;
-		int max = 0;
-		int internalMax = 0;
 
 		// Sorting SurveyResults
 		Collections.sort(surveyDetails, new SurveyResultsComparator());
 
+		// Finding max questions
+		int max = 0;
+		int internalMax = 0;
+		for (SurveyDetails survey : surveyDetails) {
+			internalMax = survey.getSurveyResponse().size();
+			if (internalMax > max)
+				max = internalMax;
+		}
+		
 		// This data needs to be written (List<Object>)
 		Map<String, List<Object>> data = new TreeMap<>();
 		List<Object> surveyDetailsToPopulate = new ArrayList<>();
 		for (SurveyDetails survey : surveyDetails) {
-			internalMax = 0;
-
 			String agentName = survey.getAgentName();
 			surveyDetailsToPopulate.add(agentName.substring(0, agentName.lastIndexOf(' ')));
 			surveyDetailsToPopulate.add(agentName.substring(agentName.lastIndexOf(' ') + 1));
@@ -395,7 +392,6 @@ public class DashboardServiceImpl implements DashboardService, InitializingBean 
 
 			surveyDetailsToPopulate.add(survey.getScore());
 			for (SurveyResponse response : survey.getSurveyResponse()) {
-				internalMax++;
 				surveyDetailsToPopulate.add(response.getAnswer());
 			}
 
@@ -421,8 +417,6 @@ public class DashboardServiceImpl implements DashboardService, InitializingBean 
 
 			data.put((++counter).toString(), surveyDetailsToPopulate);
 			surveyDetailsToPopulate = new ArrayList<>();
-			if (internalMax > max)
-				max = internalMax;
 		}
 
 		// Setting up headers
