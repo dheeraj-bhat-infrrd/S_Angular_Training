ca5645fbf91771ea26797fe1f902114755f204f7
Nishit Kannan <niskan@gmail.com>
2015-09-01 12:19:14 +0530

    Settings Locker structure

---
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/settingsmanagement/SettingsLocker.java
@@ -0,0 +1,66 @@
+package com.realtech.socialsurvey.core.services.settingsmanagement;
+
+import com.realtech.socialsurvey.core.entities.Branch;
+import com.realtech.socialsurvey.core.entities.Company;
+import com.realtech.socialsurvey.core.entities.Region;
+import com.realtech.socialsurvey.core.enums.OrganizationUnit;
+import com.realtech.socialsurvey.core.enums.SettingsForApplication;
+import com.realtech.socialsurvey.core.exception.NonFatalException;
+
+/**
+ * Lock on settings
+ */
+public interface SettingsLocker {
+
+	/**
+	 * Sets the lock settings for company
+	 * 
+	 * @param company
+	 * @param settings
+	 * @param hasBeenLocked
+	 * @return
+	 * @throws NonFatalException
+	 */
+	public Company lockSettingsValueForCompany(Company company, SettingsForApplication settings, boolean hasBeenLocked) throws NonFatalException;
+
+	/**
+	 * Sets the lock settings for region
+	 * 
+	 * @param region
+	 * @param settings
+	 * @param hasBeenLocked
+	 * @return
+	 * @throws NonFatalException
+	 */
+	public Region lockSettingsValueForRegion(Region region, SettingsForApplication settings, boolean hasBeenLocked) throws NonFatalException;
+
+	/**
+	 * Sets the lock settings for branch
+	 * 
+	 * @param branch
+	 * @param settings
+	 * @param hasBeenLocked
+	 * @return
+	 * @throws NonFatalException
+	 */
+	public Branch lockSettingsValueForBranch(Branch branch, SettingsForApplication settings, boolean hasBeenLocked) throws NonFatalException;
+	
+	
+	/**
+	 * Check if the settings value is locked
+	 * @param organizationUnit
+	 * @param currentLockValue
+	 * @param settings
+	 * @return
+	 */
+	public boolean isSettingsValueLocked(OrganizationUnit organizationUnit, long currentLockValue, SettingsForApplication settings);
+	
+	
+	/**
+	 * Checks the settings lock status
+	 * @param lockNumber
+	 * @param organizationUnit
+	 * @return
+	 */
+	public boolean checkSettingsLockStatus(int lockNumber, OrganizationUnit organizationUnit);
+}
