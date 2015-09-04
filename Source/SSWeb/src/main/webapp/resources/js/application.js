13be2b074d40a386834408906328990d2d33460c
shubham agrawal <shubham@raremile.com>
2015-09-04 14:56:43 +0530

    Fix for : SS-1121, SS-1113

--- Source/SSWeb/src/main/webapp/resources/js/application.js
+++ Source/SSWeb/src/main/webapp/resources/js/application.js
@@ -1121,6 +1121,10 @@ $(document).on('click','.da-dd-item',function(e){
 	$('#dashboard-sel').html($(this).html());
 	$('#da-dd-wrapper-profiles').slideToggle(200);
 	$('#da-dd-wrapper-profiles').perfectScrollbar('update');
+	
+	attrName = $(this).attr('data-column-type');
+	attrVal = $(this).attr('data-column-value');
+	
 	// update selected profile in session
 	updateCurrentProfile($(this).attr('data-column-type'), $(this).attr('data-column-value'));
 
