d939a35516ec457edb9f51bcb9d051b7c9c3adfc
utsav09211 <utsav.sharma@raremile.com>
2015-09-21 16:51:10 +0530

    Loop interface

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/integration/dotloop/DotloopIntegrationApi.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/integration/dotloop/DotloopIntegrationApi.java
@@ -3,6 +3,7 @@ package com.realtech.socialsurvey.core.integration.dotloop;
 import retrofit.client.Response;
 import retrofit.http.GET;
 import retrofit.http.Header;
+import retrofit.http.Path;
 
 
 /**
@@ -13,4 +14,9 @@ public interface DotloopIntegrationApi
 {
     @GET ( "/profiles")
     public Response fetchdotloopProfiles( @Header ( "authorizationHeader") String authorizationHeader );
+
+
+    @GET ( "/profile/{profile-id}/loop")
+    public Response fetchZillowReviewsByScreennameWithMaxCount( @Path ( "profile-id") String profileId );
+
 }
