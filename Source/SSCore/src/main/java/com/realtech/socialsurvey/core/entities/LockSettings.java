083969aba7aeb1a87fd02bd5f2e032b449f2ddf8
utsav09211 <utsav.sharma@raremile.com>
2015-10-01 16:42:37 +0530

    lock changes

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/LockSettings.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/LockSettings.java
@@ -1,84 +1,133 @@
 package com.realtech.socialsurvey.core.entities;
 
-public class LockSettings {
+public class LockSettings
+{
 
-	private boolean isLogoLocked;
-	private boolean isWebAddressLocked;
-	private boolean isBlogAddressLocked;
-	private boolean isWorkPhoneLocked;
-	private boolean isPersonalPhoneLocked;
-	private boolean isFaxPhoneLocked;
-	private boolean isAboutMeLocked;
-	private boolean isAddressLocked;
+    private boolean isLogoLocked;
+    private boolean isWebAddressLocked;
+    private boolean isBlogAddressLocked;
+    private boolean isWorkPhoneLocked;
+    private boolean isPersonalPhoneLocked;
+    private boolean isFaxPhoneLocked;
+    private boolean isAboutMeLocked;
+    private boolean isAddressLocked;
+    private boolean isMinScoreLocked;
 
-	public boolean getIsLogoLocked() {
-		return isLogoLocked;
-	}
 
-	public void setLogoLocked(boolean isLogoLocked) {
-		this.isLogoLocked = isLogoLocked;
-	}
+    public boolean getIsLogoLocked()
+    {
+        return isLogoLocked;
+    }
 
-	public boolean getIsWebAddressLocked() {
-		return isWebAddressLocked;
-	}
 
-	public void setWebAddressLocked(boolean isWebAddressLocked) {
-		this.isWebAddressLocked = isWebAddressLocked;
-	}
+    public void setLogoLocked( boolean isLogoLocked )
+    {
+        this.isLogoLocked = isLogoLocked;
+    }
 
-	public boolean getIsBlogAddressLocked() {
-		return isBlogAddressLocked;
-	}
 
-	public void setBlogAddressLocked(boolean isBlogAddressLocked) {
-		this.isBlogAddressLocked = isBlogAddressLocked;
-	}
+    public boolean getIsWebAddressLocked()
+    {
+        return isWebAddressLocked;
+    }
 
-	public boolean getIsWorkPhoneLocked() {
-		return isWorkPhoneLocked;
-	}
 
-	public void setWorkPhoneLocked(boolean isWorkPhoneLocked) {
-		this.isWorkPhoneLocked = isWorkPhoneLocked;
-	}
+    public void setWebAddressLocked( boolean isWebAddressLocked )
+    {
+        this.isWebAddressLocked = isWebAddressLocked;
+    }
 
-	public boolean getIsPersonalPhoneLocked() {
-		return isPersonalPhoneLocked;
-	}
 
-	public void setPersonalPhoneLocked(boolean isPersonalPhoneLocked) {
-		this.isPersonalPhoneLocked = isPersonalPhoneLocked;
-	}
+    public boolean getIsBlogAddressLocked()
+    {
+        return isBlogAddressLocked;
+    }
 
-	public boolean getIsFaxPhoneLocked() {
-		return isFaxPhoneLocked;
-	}
 
-	public void setFaxPhoneLocked(boolean isFaxPhoneLocked) {
-		this.isFaxPhoneLocked = isFaxPhoneLocked;
-	}
+    public void setBlogAddressLocked( boolean isBlogAddressLocked )
+    {
+        this.isBlogAddressLocked = isBlogAddressLocked;
+    }
 
-	public boolean getIsAboutMeLocked() {
-		return isAboutMeLocked;
-	}
 
-	public void setAboutMeLocked(boolean isAboutMeLocked) {
-		this.isAboutMeLocked = isAboutMeLocked;
-	}
+    public boolean getIsWorkPhoneLocked()
+    {
+        return isWorkPhoneLocked;
+    }
 
-	public boolean getIsAddressLocked() {
-		return isAddressLocked;
-	}
 
-	public void setAddressLocked(boolean isAddressLocked) {
-		this.isAddressLocked = isAddressLocked;
-	}
+    public void setWorkPhoneLocked( boolean isWorkPhoneLocked )
+    {
+        this.isWorkPhoneLocked = isWorkPhoneLocked;
+    }
 
-	@Override
-	public String toString() {
-		return "LockSettings [isLogoLocked=" + isLogoLocked + ", isWebAddressLocked=" + isWebAddressLocked + ", isBlogAddressLocked="
-				+ isBlogAddressLocked + ", isWorkPhoneLocked=" + isWorkPhoneLocked + ", isPersonalPhoneLocked=" + isPersonalPhoneLocked
-				+ ", isFaxPhoneLocked=" + isFaxPhoneLocked + ", isAboutMeLocked=" + isAboutMeLocked + ", isAddressLocked=" + isAddressLocked + "]";
-	}
+
+    public boolean getIsPersonalPhoneLocked()
+    {
+        return isPersonalPhoneLocked;
+    }
+
+
+    public void setPersonalPhoneLocked( boolean isPersonalPhoneLocked )
+    {
+        this.isPersonalPhoneLocked = isPersonalPhoneLocked;
+    }
+
+
+    public boolean getIsFaxPhoneLocked()
+    {
+        return isFaxPhoneLocked;
+    }
+
+
+    public void setFaxPhoneLocked( boolean isFaxPhoneLocked )
+    {
+        this.isFaxPhoneLocked = isFaxPhoneLocked;
+    }
+
+
+    public boolean getIsAboutMeLocked()
+    {
+        return isAboutMeLocked;
+    }
+
+
+    public void setAboutMeLocked( boolean isAboutMeLocked )
+    {
+        this.isAboutMeLocked = isAboutMeLocked;
+    }
+
+
+    public boolean getIsAddressLocked()
+    {
+        return isAddressLocked;
+    }
+
+
+    public void setAddressLocked( boolean isAddressLocked )
+    {
+        this.isAddressLocked = isAddressLocked;
+    }
+
+
+    public boolean isMinScoreLocked()
+    {
+        return isMinScoreLocked;
+    }
+
+
+    public void setMinScoreLocked( boolean isMinScoreLocked )
+    {
+        this.isMinScoreLocked = isMinScoreLocked;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return "LockSettings [isLogoLocked=" + isLogoLocked + ", isWebAddressLocked=" + isWebAddressLocked
+            + ", isBlogAddressLocked=" + isBlogAddressLocked + ", isWorkPhoneLocked=" + isWorkPhoneLocked
+            + ", isPersonalPhoneLocked=" + isPersonalPhoneLocked + ", isFaxPhoneLocked=" + isFaxPhoneLocked
+            + ", isAboutMeLocked=" + isAboutMeLocked + ", isAddressLocked=" + isAddressLocked + "]";
+    }
 }
