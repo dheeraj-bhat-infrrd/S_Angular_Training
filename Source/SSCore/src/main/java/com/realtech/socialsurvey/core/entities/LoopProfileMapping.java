b6bd87cbf8c887bc1995f6877014d20ad0f1cfc6
utsav09211 <utsav.sharma@raremile.com>
2015-10-15 16:15:56 +0530

    Fix for dotloop at different hierarchy

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/LoopProfileMapping.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/LoopProfileMapping.java
@@ -9,112 +9,227 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
+
 /**
  * The persistent class for the crm_master database table.
  */
 @Entity
-@Table(name = "DOTLOOP_PROFILE_LOOP_MAPPING")
-@NamedQuery(name = "LoopProfileMapping.findAll", query = "SELECT lpm FROM LoopProfileMapping lpm")
-public class LoopProfileMapping implements Serializable {
-	private static final long serialVersionUID = 1L;
+@Table ( name = "DOTLOOP_PROFILE_LOOP_MAPPING")
+@NamedQuery ( name = "LoopProfileMapping.findAll", query = "SELECT lpm FROM LoopProfileMapping lpm")
+public class LoopProfileMapping implements Serializable
+{
+    private static final long serialVersionUID = 1L;
 
-	public LoopProfileMapping() {}
 
-	@Id
-	@Column(name = "PROFILE_LOOP_MAPPING_ID")
-	private long id;
+    public LoopProfileMapping()
+    {}
 
-	@Column(name = "PROFILE_ID")
-	private String profileId;
+    @Id
+    @Column ( name = "PROFILE_LOOP_MAPPING_ID")
+    private long id;
 
-	@Column(name = "PROFILE_LOOP_ID")
-	private String loopId;
+    @Column ( name = "PROFILE_ID")
+    private String profileId;
 
-	@Column(name = "PROFILE_LOOP_VIEW_ID")
-	private String loopViewId;
+    @Column ( name = "PROFILE_LOOP_ID")
+    private String loopId;
 
-	@Column(name = "LOOP_CLOSED_TIME")
-	private Timestamp loopClosedTime;
+    @Column ( name = "PROFILE_LOOP_VIEW_ID")
+    private String loopViewId;
 
-	@Transient
-	private String loopName;
-	@Transient
-	private String loopStatus;
-	@Transient
-	private long createdBy;
-	@Transient
-	private String lastUpdated;
+    @Column ( name = "LOOP_CLOSED_TIME")
+    private Timestamp loopClosedTime;
 
-	public long getId() {
-		return id;
-	}
+    @Column ( name = "COMPANY_ID")
+    private long companyId;
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    @Column ( name = "REGION_ID")
+    private long regionId;
 
-	public String getProfileId() {
-		return profileId;
-	}
+    @Column ( name = "BRANCH_ID")
+    private long branchId;
 
-	public void setProfileId(String profileId) {
-		this.profileId = profileId;
-	}
+    @Column ( name = "AGENT_ID")
+    private long agentId;
 
-	public String getLoopId() {
-		return loopId;
-	}
+    @Column ( name = "COLLECTION_NAME")
+    private String collectionName;
 
-	public void setLoopId(String loopId) {
-		this.loopId = loopId;
-	}
+    @Transient
+    private String loopName;
+    @Transient
+    private String loopStatus;
+    @Transient
+    private long createdBy;
+    @Transient
+    private String lastUpdated;
 
-	public String getLoopViewId() {
-		return loopViewId;
-	}
 
-	public void setLoopViewId(String loopViewId) {
-		this.loopViewId = loopViewId;
-	}
+    public long getId()
+    {
+        return id;
+    }
 
-	public Timestamp getLoopClosedTime() {
-		return loopClosedTime;
-	}
 
-	public void setLoopClosedTime(Timestamp loopClosedTime) {
-		this.loopClosedTime = loopClosedTime;
-	}
+    public void setId( long id )
+    {
+        this.id = id;
+    }
 
-	public String getLoopName() {
-		return loopName;
-	}
 
-	public void setLoopName(String loopName) {
-		this.loopName = loopName;
-	}
+    public String getProfileId()
+    {
+        return profileId;
+    }
 
-	public String getLoopStatus() {
-		return loopStatus;
-	}
 
-	public void setLoopStatus(String loopStatus) {
-		this.loopStatus = loopStatus;
-	}
+    public void setProfileId( String profileId )
+    {
+        this.profileId = profileId;
+    }
 
-	public long getCreatedBy() {
-		return createdBy;
-	}
 
-	public void setCreatedBy(long createdBy) {
-		this.createdBy = createdBy;
-	}
+    public String getLoopId()
+    {
+        return loopId;
+    }
 
-	public String getLastUpdated() {
-		return lastUpdated;
-	}
 
-	public void setLastUpdated(String lastUpdated) {
-		this.lastUpdated = lastUpdated;
-	}
+    public void setLoopId( String loopId )
+    {
+        this.loopId = loopId;
+    }
+
+
+    public String getLoopViewId()
+    {
+        return loopViewId;
+    }
+
+
+    public void setLoopViewId( String loopViewId )
+    {
+        this.loopViewId = loopViewId;
+    }
+
+
+    public Timestamp getLoopClosedTime()
+    {
+        return loopClosedTime;
+    }
+
+
+    public void setLoopClosedTime( Timestamp loopClosedTime )
+    {
+        this.loopClosedTime = loopClosedTime;
+    }
+
+
+    public String getLoopName()
+    {
+        return loopName;
+    }
+
+
+    public void setLoopName( String loopName )
+    {
+        this.loopName = loopName;
+    }
+
+
+    public String getLoopStatus()
+    {
+        return loopStatus;
+    }
+
+
+    public void setLoopStatus( String loopStatus )
+    {
+        this.loopStatus = loopStatus;
+    }
+
+
+    public long getCreatedBy()
+    {
+        return createdBy;
+    }
+
+
+    public void setCreatedBy( long createdBy )
+    {
+        this.createdBy = createdBy;
+    }
+
+
+    public String getLastUpdated()
+    {
+        return lastUpdated;
+    }
+
+
+    public void setLastUpdated( String lastUpdated )
+    {
+        this.lastUpdated = lastUpdated;
+    }
+
+
+    public long getCompanyId()
+    {
+        return companyId;
+    }
+
+
+    public void setCompanyId( long companyId )
+    {
+        this.companyId = companyId;
+    }
+
+
+    public long getRegionId()
+    {
+        return regionId;
+    }
+
+
+    public void setRegionId( long regionId )
+    {
+        this.regionId = regionId;
+    }
+
+
+    public long getBranchId()
+    {
+        return branchId;
+    }
+
+
+    public void setBranchId( long branchId )
+    {
+        this.branchId = branchId;
+    }
+
+
+    public long getAgentId()
+    {
+        return agentId;
+    }
+
+
+    public void setAgentId( long agentId )
+    {
+        this.agentId = agentId;
+    }
+
+
+    public String getCollectionName()
+    {
+        return collectionName;
+    }
+
+
+    public void setCollectionName( String collectionName )
+    {
+        this.collectionName = collectionName;
+    }
 
 }
