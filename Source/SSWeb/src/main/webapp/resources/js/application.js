3ce659abdc863ed1dd811da5ded60c0094b5bea8
ghanashyam <ghanashyamvtatti@gmail.com>
2015-10-16 21:50:02 +0530

    Added functionality to generate widgets for individuals The Generate Widget button can be found in Manage Team > Edit Team

--- Source/SSWeb/src/main/webapp/resources/js/application.js
+++ Source/SSWeb/src/main/webapp/resources/js/application.js
@@ -4461,7 +4461,6 @@ $(document).on('click', '#page-next.paginate-button', function(){
 		}
 	}
 });
-
 function updatePaginateButtons() {
 	var numFound = $('#u-tbl-header').attr('data-num-found');
 	if (numFound > userBatchSize) {
@@ -8152,6 +8151,16 @@ function editProfileUrl() {
 	}, true);
 	
 }
+// Get all the required elements and show popup
+function generateWidget(iden, profileLevel) {
+	window.open("./showwidgetpage.do?profileLevel=" + profileLevel + "&iden="
+			+ iden, "_blank", "width=800,height=600,scrollbars=yes");
+}
+
+function getWidgetGenerationPage(){
+	var url = "./getwidgetselectionpage.do";
+	callAjaxGET(url, paintEditSection, true);
+}
 
 /**
  * Warning popup
@@ -8165,7 +8174,6 @@ function createEditProfileUrlPopup(header, body) {
 	$('#overlay-cancel').html("Cancel");
 	$('#overlay-continue').off();
 	$('#overlay-continue').click(function(){
-		//Add method to show the zillow type popup
 		$('#overlay-continue').unbind('click');
 		$('#overlay-cancel').unbind('click');
 		updateProfileUrl();
@@ -8175,7 +8183,6 @@ function createEditProfileUrlPopup(header, body) {
 	$('#overlay-main').show();
 	disableBodyScroll();
 }
-
 function updateProfileUrl(){
 	window.open("./editprofileurl.do","_blank", "width=800,height=600,scrollbars=yes");
 }
