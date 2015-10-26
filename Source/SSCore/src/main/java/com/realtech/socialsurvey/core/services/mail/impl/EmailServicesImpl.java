206992e0436eea328b5c76098ad585571ac3fc2e
Nishit Kannan <niskan@gmail.com>
2015-10-26 15:33:51 -0700

    Organised import

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/mail/impl/EmailServicesImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/mail/impl/EmailServicesImpl.java
@@ -8,7 +8,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.lang.WordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
