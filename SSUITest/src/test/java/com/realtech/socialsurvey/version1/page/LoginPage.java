04a35ce5c4f378367bd8fc5822d76f75aea380e9
shubham agrawal <shubham@raremile.com>
2015-10-29 15:11:04 +0530

    Changes in selenium.

--- SSUITest/src/test/java/com/realtech/socialsurvey/version1/page/LoginPage.java
+++ SSUITest/src/test/java/com/realtech/socialsurvey/version1/page/LoginPage.java
@@ -1,22 +1,33 @@
 package com.realtech.socialsurvey.version1.page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-
 
 
 public class LoginPage extends BasePage
 {
-    
+
     public static final String LOGIN_PAGE_TITLE = "Login";
+    
+    public static final String INVALID_CREDENTIALS_MESSAGE = "INVALID EMAIL ADDRESS OR PASSWORD";
+    public static final String ERROR_LOCATOR = "id=err-nw-txt";
+
 
     public LoginPage( WebDriver driver )
     {
         super( driver );
-        if (!getTitle().equals(LOGIN_PAGE_TITLE)) {
-            Assert.fail("This is not login page. Current page is: " + getTitle());
+        if ( !getTitle().equals( LOGIN_PAGE_TITLE ) ) {
+            Assert.fail( "This is not login page. Current page is: " + getTitle() );
         }
-        
+
         waitForAjax();
+    }
+
+
+    public String getErrorMessage()
+    {
+        WebElement message = getElement( ERROR_LOCATOR );
+        return message.getText();
     }
 }
