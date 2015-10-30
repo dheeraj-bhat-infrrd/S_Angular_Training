ddad96f0c57d5a04b76749ed281bb1d3acc5f24d
shubham agrawal <shubham@raremile.com>
2015-10-30 16:55:59 +0530

    Test cases written for login page.

--- SSUITest/src/test/java/com/realtech/socialsurvey/version1/page/LoginPage.java
+++ SSUITest/src/test/java/com/realtech/socialsurvey/version1/page/LoginPage.java
@@ -1,6 +1,8 @@
 package com.realtech.socialsurvey.version1.page;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 
@@ -8,8 +10,12 @@ public class LoginPage extends BasePage
 {
 
     public static final String LOGIN_PAGE_TITLE = "Login";
-    
+
     public static final String INVALID_CREDENTIALS_MESSAGE = "INVALID EMAIL ADDRESS OR PASSWORD";
+
+    public static final String USER_NAME_LOCATOR = "id=login-user-id";
+    public static final String USER_PASSWORD_LOCATOR = "id=login-pwd";
+    public static final String FORGOT_PASSWORD_LOCATOR = "link=Forgot Password";
 
 
     public LoginPage( WebDriver driver )
@@ -21,5 +27,40 @@ public class LoginPage extends BasePage
 
         waitForAjax();
     }
+
+
+    public DashboardPage loginUser( String userNameString, String passwordString )
+    {
+
+        WebElement userName = getElement( USER_NAME_LOCATOR );
+        userName.clear();
+        userName.sendKeys( userNameString );
+
+        WebElement password = getElement( USER_PASSWORD_LOCATOR );
+        password.clear();
+        password.sendKeys( passwordString );
+
+        //Press enter to login
+        password.sendKeys( Keys.ENTER );
+
+
+        waitForAjax();
+        return new DashboardPage( driver );
+    }
+
+
+    public ForgotPasswordPage clickForgotPasswordLink()
+    {
+        navigateToForgotPasswordPage();
+        waitForAjax();
+        return new ForgotPasswordPage( driver );
+    }
+
+
+    public void navigateToForgotPasswordPage()
+    {
+        navigateToPage( FORGOT_PASSWORD_LOCATOR );
+    }
+
 
 }
