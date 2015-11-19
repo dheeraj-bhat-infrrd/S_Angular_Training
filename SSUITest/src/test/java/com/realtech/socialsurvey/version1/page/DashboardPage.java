66b97f23359ce31920484a2157f28c53b0b8f400
shubham agrawal <shubham@raremile.com>
2015-11-19 17:15:17 +0530

    Added test case in dashboard page.

--- SSUITest/src/test/java/com/realtech/socialsurvey/version1/page/DashboardPage.java
+++ SSUITest/src/test/java/com/realtech/socialsurvey/version1/page/DashboardPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
+
 public class DashboardPage extends BasePage
 {
 
@@ -13,10 +14,24 @@ public class DashboardPage extends BasePage
     public static final String CONNECTTO_LINKEDIN_BUTTON_LOCATOR = "xpath=//div[contains(text(), 'Connect to Linkedin')]";
     public static final String CONNECTTO_FACEBOOK_BUTTON_LOCATOR = "xpath=//div[contains(text(), 'Connect to Facebook')]";
     public static final String CONNECTTO_GOOGLE_BUTTON_LOCATOR = "xpath=//div[contains(text(), 'Connect to Google+')]";
-    public static final String VIEW_ALL_INCOMOPLETED_SURVEY_LOCATOR="xpath=//*[@id='incomplete-survey-header']/div[contains(@class, 'dash-sur-link')]";
-    public static final String VIEW_AS_DROPDOWN_LOCATOR="id=dashboard-sel";
-    public static final String VIEW_AS_DROPDOWN_OPTION_LOCATOR="xpath=//*[contains(text(), '@viewasname') and contains(@class, 'da-dd-item')]";
-    
+    public static final String CONNECTTO_YELP_BUTTON_LOCATOR = "xpath=//div[contains(text(), 'Connect to Yelp')]";
+    public static final String CONNECTTO_ZILLOW_BUTTON_LOCATOR = "xpath=//div[contains(text(), 'Connect to Zillow')]";
+    public static final String ENTER_LICENSE_DETAILS_BUTTON_LOCATOR = "xpath=//div[contains(text(), 'Enter license details')]";
+    public static final String ENTER_HOBBIES_BUTTON_LOCATOR = "xpath=//div[contains(text(), 'Enter hobbies')]";
+    public static final String ENTER_ACHIEVEMENTS_BUTTON_LOCATOR = "xpath=//div[contains(text(), 'Enter achievements')]";
+    public static final String VIEW_ALL_INCOMOPLETED_SURVEY_LOCATOR = "xpath=//*[@id='incomplete-survey-header']/div[contains(@class, 'dash-sur-link')]";
+    public static final String VIEW_AS_DROPDOWN_LOCATOR = "id=dashboard-sel";
+    public static final String VIEW_AS_DROPDOWN_OPTION_LOCATOR = "xpath=//*[contains(text(), '@viewasname') and contains(@class, 'da-dd-item')]";
+
+    //Header link locators
+    public static final String EDIT_PROFILE_LOCATOR = "link=Edit Profile";
+    public static final String EDIT_SURVEY_LOCATOR = "link=Edit Survey";
+    public static final String SOCIAL_MONITOR_LOCATOR = "link=Social Monitor";
+    public static final String MANAGE_TEAM_LOCATOR = "link=Manage Team";
+    public static final String CONFIGURE_SETTINGS_LOCATOR = "link=Configure";
+    public static final String HELP_LOCATOR = "link=Help";
+
+
     public DashboardPage( WebDriver driver )
     {
         super( driver );
@@ -26,45 +41,179 @@ public class DashboardPage extends BasePage
 
         waitForAjax();
     }
-    
-    public void clickSendSurveyPopupClose () {
+
+
+    public void clickSendSurveyPopupClose()
+    {
         WebElement closeBtn = getElement( POPUP_CLOSE_LOCATOR );
         closeBtn.click();
     }
-    
-    public void clickSendSurveyButtonDashboard () {
+
+
+    public void clickSendSurveyButtonDashboard()
+    {
         WebElement sendSurveyBtn = getElement( SENDSURVEY_BUTTON_LOCATOR );
         sendSurveyBtn.click();
     }
-    
-    public void clickConnectToLinkedIn() {
+
+
+    public void clickConnectToLinkedIn()
+    {
         WebElement linkedInConnectBtn = getElement( CONNECTTO_LINKEDIN_BUTTON_LOCATOR );
         linkedInConnectBtn.click();
     }
-    
-    public void clickConnectToFacebook() {
-        WebElement facebookConnectBtn = getElement( CONNECTTO_LINKEDIN_BUTTON_LOCATOR );
+
+
+    public void clickConnectToFacebook()
+    {
+        WebElement facebookConnectBtn = getElement( CONNECTTO_FACEBOOK_BUTTON_LOCATOR );
         facebookConnectBtn.click();
     }
-    
-    public void clickViewAllIncompleteSurvey() {
+
+
+    public void clickViewAllIncompleteSurvey()
+    {
         WebElement incompleteSurveyBtn = getElement( VIEW_ALL_INCOMOPLETED_SURVEY_LOCATOR );
         incompleteSurveyBtn.click();
     }
-    public void clickConnectToGoogle(){
+
+
+    public void clickConnectToGoogle()
+    {
         WebElement googlePlusBtn = getElement( CONNECTTO_GOOGLE_BUTTON_LOCATOR );
         googlePlusBtn.click();
     }
-    
-    public void clickDashboardDropDown() {
+
+
+    public void clickConnectToYelp()
+    {
+        WebElement yelpBtn = getElement( CONNECTTO_YELP_BUTTON_LOCATOR );
+        yelpBtn.click();
+    }
+
+
+    public void clickConnectToZillow()
+    {
+        WebElement yelpBtn = getElement( CONNECTTO_ZILLOW_BUTTON_LOCATOR );
+        yelpBtn.click();
+    }
+
+
+    public void clickEnterLicenseDetails()
+    {
+        WebElement enterLicenseDetailsBtn = getElement( ENTER_LICENSE_DETAILS_BUTTON_LOCATOR );
+        enterLicenseDetailsBtn.click();
+    }
+
+
+    public void clickEnterHobbies()
+    {
+        WebElement enterHobbiesBtn = getElement( ENTER_HOBBIES_BUTTON_LOCATOR );
+        enterHobbiesBtn.click();
+    }
+
+
+    public void clickEnterAchievements()
+    {
+        WebElement enterAchievementssBtn = getElement( ENTER_ACHIEVEMENTS_BUTTON_LOCATOR );
+        enterAchievementssBtn.click();
+    }
+
+
+    public void clickDashboardDropDown()
+    {
         WebElement dashboardDropdownBtn = getElement( VIEW_AS_DROPDOWN_LOCATOR );
         dashboardDropdownBtn.click();
     }
-    
-    public void selectViewAsOption(String viewAsName) {
+
+
+    public void selectViewAsOption( String viewAsName )
+    {
         WebElement viewAsOption = getElement( VIEW_AS_DROPDOWN_OPTION_LOCATOR.replace( "@viewasname", viewAsName ) );
         viewAsOption.click();
         waitForAjax();
     }
 
+
+    public EditProfilePage clickEditProfilePage()
+    {
+        navigateToEditProfilePage();
+        waitForAjax();
+        return new EditProfilePage( driver );
+    }
+
+
+    public void navigateToEditProfilePage()
+    {
+        navigateToPage( EDIT_PROFILE_LOCATOR );
+    }
+
+
+    public EditSurveyPage clickEditSurveyPage()
+    {
+        navigateToEditSurveyPage();
+        waitForAjax();
+        return new EditSurveyPage( driver );
+    }
+
+
+    public void navigateToEditSurveyPage()
+    {
+        navigateToPage( EDIT_SURVEY_LOCATOR );
+    }
+
+
+    public SocialMonitorPage clickSocialMonitorPage()
+    {
+        navigateToSocialMonitorPage();
+        waitForAjax();
+        return new SocialMonitorPage( driver );
+    }
+
+
+    public void navigateToSocialMonitorPage()
+    {
+        navigateToPage( SOCIAL_MONITOR_LOCATOR );
+    }
+
+
+    public EditTeamPage clickManageTeamPage()
+    {
+        navigateToManageTeamPage();
+        waitForAjax();
+        return new EditTeamPage( driver );
+    }
+
+
+    public void navigateToManageTeamPage()
+    {
+        navigateToPage( MANAGE_TEAM_LOCATOR );
+    }
+
+
+    public SettingsPage clickConfigureSettingsPage()
+    {
+        navigateToConfigureSettingsPage();
+        waitForAjax();
+        return new SettingsPage( driver );
+    }
+
+
+    public void navigateToConfigureSettingsPage()
+    {
+        navigateToPage( CONFIGURE_SETTINGS_LOCATOR );
+    }
+    
+    public HelpPage clickHelpPage()
+    {
+        navigateToHelpPage();
+        waitForAjax();
+        return new HelpPage( driver );
+    }
+
+
+    public void navigateToHelpPage()
+    {
+        navigateToPage( HELP_LOCATOR);
+    }
 }
--- SSUITest/src/test/java/com/realtech/socialsurvey/version1/testcase/TestUserDashboard.java
+++ SSUITest/src/test/java/com/realtech/socialsurvey/version1/testcase/TestUserDashboard.java
@@ -212,4 +212,349 @@ public class TestUserDashboard extends BaseTestCase
                 + e.getMessage() );
         }
     }
+
+
+    @Test ( groups = "userdashboard", testName = "TCSSUD-12")
+    public void testUserDashboardConnectToYelpClick()
+    {
+        LOG.trace( "\n\n*** Started Testing: testUserDashboardConnectToYelpClick" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickConnectToYelp();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testUserDashboardConnectToYelpClick" );
+            Assert.fail( "Exception Occurred While Testing: testUserDashboardConnectToYelpClick: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userdashboard", testName = "TCSSUD-13")
+    public void testUserDashboardConnectToZillowClick()
+    {
+        LOG.trace( "\n\n*** Started Testing: testUserDashboardConnectToZillowClick" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickConnectToZillow();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testUserDashboardConnectToZillowClick" );
+            Assert.fail( "Exception Occurred While Testing: testUserDashboardConnectToZillowClick: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userdashboard", testName = "TCSSUD-14")
+    public void testUserDashboardEnterLicenseDetailsClick()
+    {
+        LOG.trace( "\n\n*** Started Testing: testUserDashboardEnterLicenseDetailsClick" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickEnterLicenseDetails();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testUserDashboardEnterLicenseDetailsClick" );
+            Assert.fail( "Exception Occurred While Testing: testUserDashboardEnterLicenseDetailsClick: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userdashboard", testName = "TCSSUD-15")
+    public void testUserDashboardEnterHobbiesClick()
+    {
+        LOG.trace( "\n\n*** Started Testing: testUserDashboardEnterHobbiesClick" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickEnterHobbies();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testUserDashboardEnterHobbiesClick" );
+            Assert.fail( "Exception Occurred While Testing: testUserDashboardEnterHobbiesClick: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userdashboard", testName = "TCSSUD-16")
+    public void testUserDashboardEnterAchievementsClick()
+    {
+        LOG.trace( "\n\n*** Started Testing: testUserDashboardEnterAchievementsClick" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickEnterAchievements();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testUserDashboardEnterAchievementsClick" );
+            Assert.fail( "Exception Occurred While Testing: testUserDashboardEnterAchievementsClick: " + e.getMessage() );
+        }
+    }
+
+
+    /**
+     * Test the user header links
+     */
+    @Test ( groups = "userheader", testName = "TCSSUD-17")
+    public void testClickEditProfileIndividual()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickEditProfileIndividual" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickEditProfilePage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickEditProfileIndividual" );
+            Assert.fail( "Exception Occurred While Testing: testClickEditProfileIndividual: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-18")
+    public void testClickEditProfileBranchAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickEditProfileBranchAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.BRANCH_USER_NAME,
+                GlobalConstants.BRANCH_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickEditProfilePage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickEditProfileBranchAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickEditProfileBranchAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-19")
+    public void testClickEditProfileRegionAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickEditProfileRegionAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.REGION_USER_NAME,
+                GlobalConstants.REGION_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickEditProfilePage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickEditProfileRegionAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickEditProfileRegionAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-20")
+    public void testClickEditProfileCompanyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickEditProfileCompanyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickEditProfilePage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickEditProfileCompanyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickEditProfileCompanyAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-21")
+    public void testClickEditSurveyCompanyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickEditSurveyCompanyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickEditSurveyPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickEditSurveyCompanyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickEditSurveyCompanyAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-22")
+    public void testClickSocialMonitorCompanyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickSocialMonitorCompanyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickSocialMonitorPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickSocialMonitorCompanyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickSocialMonitorCompanyAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-23")
+    public void testClickManageTeamBranchAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickManageTeamBranchAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.BRANCH_USER_NAME,
+                GlobalConstants.BRANCH_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickManageTeamPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickManageTeamBranchAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickManageTeamBranchAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-24")
+    public void testClickManageTeamRegionAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickManageTeamRegionAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.REGION_USER_NAME,
+                GlobalConstants.REGION_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickManageTeamPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickManageTeamRegionAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickManageTeamRegionAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-25")
+    public void testClickManageTeamCompanyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickSocialMonitorCompanyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickManageTeamPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickSocialMonitorCompanyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickSocialMonitorCompanyAdmin: " + e.getMessage() );
+        }
+    }
+    
+    @Test ( groups = "userheader", testName = "TCSSUD-26")
+    public void testClickHelpIndividual()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickHelpIndividual" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickHelpPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickHelpIndividual" );
+            Assert.fail( "Exception Occurred While Testing: testClickHelpIndividual: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-27")
+    public void testClickHelpBranchAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickHelpBranchAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.BRANCH_USER_NAME,
+                GlobalConstants.BRANCH_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickHelpPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickHelpBranchAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickHelpBranchAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-28")
+    public void testClickHelpRegionAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickHelpRegionAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.REGION_USER_NAME,
+                GlobalConstants.REGION_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickHelpPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickHelpRegionAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickHelpRegionAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-29")
+    public void testClickHelpCompanyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickHelpCompanyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickHelpPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickHelpCompanyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickHelpCompanyAdmin: " + e.getMessage() );
+        }
+    }
+
 }
