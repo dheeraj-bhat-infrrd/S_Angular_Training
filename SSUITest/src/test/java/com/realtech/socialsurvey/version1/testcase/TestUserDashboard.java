5d068e935ef3a71fce697d4b462c7e3acebb10b9
shubham agrawal <shubham@raremile.com>
2015-11-20 15:55:28 +0530

    Added test cases for dashboard page

--- SSUITest/src/test/java/com/realtech/socialsurvey/version1/testcase/TestUserDashboard.java
+++ SSUITest/src/test/java/com/realtech/socialsurvey/version1/testcase/TestUserDashboard.java
@@ -309,8 +309,10 @@ public class TestUserDashboard extends BaseTestCase
     }
 
 
+    //Test the user header links
+
     /**
-     * Test the user header links
+     *Test edit profile for all profile types 
      */
     @Test ( groups = "userheader", testName = "TCSSUD-17")
     public void testClickEditProfileIndividual()
@@ -388,6 +390,9 @@ public class TestUserDashboard extends BaseTestCase
     }
 
 
+    /**
+     * Test Edit survey click for company admin
+     */
     @Test ( groups = "userheader", testName = "TCSSUD-21")
     public void testClickEditSurveyCompanyAdmin()
     {
@@ -407,6 +412,9 @@ public class TestUserDashboard extends BaseTestCase
     }
 
 
+    /**
+     * Test social monitor click for company admin
+     */
     @Test ( groups = "userheader", testName = "TCSSUD-22")
     public void testClickSocialMonitorCompanyAdmin()
     {
@@ -426,6 +434,9 @@ public class TestUserDashboard extends BaseTestCase
     }
 
 
+    /**
+     * Test manage team click for all profile types
+     */
     @Test ( groups = "userheader", testName = "TCSSUD-23")
     public void testClickManageTeamBranchAdmin()
     {
@@ -481,7 +492,11 @@ public class TestUserDashboard extends BaseTestCase
             Assert.fail( "Exception Occurred While Testing: testClickSocialMonitorCompanyAdmin: " + e.getMessage() );
         }
     }
-    
+
+
+    /**
+     * Test help page link click for all profiles
+     */
     @Test ( groups = "userheader", testName = "TCSSUD-26")
     public void testClickHelpIndividual()
     {
@@ -557,4 +572,366 @@ public class TestUserDashboard extends BaseTestCase
         }
     }
 
+
+    /**
+     * Test complaint resolution click
+     */
+    @Test ( groups = "userheader", testName = "TCSSUD-30")
+    public void testClickComplaintResolutionCompanyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickComplaintResolutionCompanyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickComplaintResolutionPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickComplaintResolutionCompanyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickComplaintResolutionCompanyAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    /**
+     * Test update password page link click for all profiles
+     */
+    @Test ( groups = "userheader", testName = "TCSSUD-31")
+    public void testClickUpdatePasswordIndividual()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickUpdatePasswordIndividual" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickUpdatePasswordPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickUpdatePasswordIndividual" );
+            Assert.fail( "Exception Occurred While Testing: testClickUpdatePasswordIndividual: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-32")
+    public void testClickUpdatePasswordBranchAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickUpdatePasswordBranchAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.BRANCH_USER_NAME,
+                GlobalConstants.BRANCH_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickUpdatePasswordPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickUpdatePasswordBranchAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickUpdatePasswordBranchAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-33")
+    public void testClickUpdatePasswordRegionAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickUpdatePasswordRegionAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.REGION_USER_NAME,
+                GlobalConstants.REGION_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickUpdatePasswordPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickUpdatePasswordRegionAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickUpdatePasswordRegionAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-34")
+    public void testClickUpdatePasswordCompanyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickUpdatePasswordCompanyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickUpdatePasswordPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickUpdatePasswordCompanyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickUpdatePasswordCompanyAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    /**
+     * Test email settings click
+     */
+    @Test ( groups = "userheader", testName = "TCSSUD-35")
+    public void testClickEmailSettingsCompanyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickEmailSettingsCompanyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickEmailSettingsPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickEmailSettingsCompanyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickEmailSettingsCompanyAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    /**
+     * Test apps page link click for all profiles
+     */
+    @Test ( groups = "userheader", testName = "TCSSUD-36")
+    public void testClickAppSettingsIndividual()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickAppSettingsIndividual" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickAppSettingsPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickAppSettingsIndividual" );
+            Assert.fail( "Exception Occurred While Testing: testClickAppSettingsIndividual: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-37")
+    public void testClickAppSettingsBranchAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickAppSettingsBranchAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.BRANCH_USER_NAME,
+                GlobalConstants.BRANCH_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickAppSettingsPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickAppSettingsBranchAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickAppSettingsBranchAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-38")
+    public void testClickAppSettingsRegionAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickAppSettingsRegionAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.REGION_USER_NAME,
+                GlobalConstants.REGION_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickAppSettingsPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickAppSettingsRegionAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickAppSettingsRegionAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-39")
+    public void testClickAppSettingsCompanyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickAppSettingsCompanyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickAppSettingsPage();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickAppSettingsCompanyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickAppSettingsCompanyAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    /**
+     * Test apps page link click for all profiles
+     */
+    @Test ( groups = "userheader", testName = "TCSSUD-40")
+    public void testClickLogoutIndividual()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickLogoutIndividual" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickLogoutButton();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickLogoutIndividual" );
+            Assert.fail( "Exception Occurred While Testing: testClickLogoutIndividual: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-41")
+    public void testClickLogoutBranchAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickLogoutBranchAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.BRANCH_USER_NAME,
+                GlobalConstants.BRANCH_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickLogoutButton();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickLogoutBranchAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickLogoutBranchAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-42")
+    public void testClickLogoutRegionAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickLogoutRegionAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.REGION_USER_NAME,
+                GlobalConstants.REGION_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickLogoutButton();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickLogoutRegionAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickLogoutRegionAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    @Test ( groups = "userheader", testName = "TCSSUD-43")
+    public void testClickLogoutCompanyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testClickLogoutCompanyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickLogoutButton();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testClickLogoutCompanyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testClickLogoutCompanyAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    //Test send survey after login with individual login
+    @Test ( groups = "userdashboard", testName = "TCSSUD-44")
+    public void testSendSurveyIndividual()
+    {
+        LOG.trace( "\n\n*** Started Testing: testSendSurveyIndividual" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.INDIVIDUAL_USER_NAME,
+                GlobalConstants.INDIVIDUAL_USER_PASSWORD );
+            dashboardPage.fillSurveyDetails( 1, GlobalConstants.CUSTOMER_FIRST_NAME, GlobalConstants.CUSTOMER_LAST_NAME,
+                GlobalConstants.CUSTOMER_EMAILID, null );
+            dashboardPage.clickSendSurveyButton();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testSendSurveyIndividual" );
+            Assert.fail( "Exception Occurred While Testing: testSendSurveyIndividual: " + e.getMessage() );
+        }
+    }
+
+
+    //Test send survey after login with admin login
+    @Test ( groups = "userdashboard", testName = "TCSSUD-45")
+    public void testSendSurveyAdmin()
+    {
+        LOG.trace( "\n\n*** Started Testing: testSendSurveyAdmin" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.fillSurveyDetails( 1, GlobalConstants.CUSTOMER_FIRST_NAME, GlobalConstants.CUSTOMER_LAST_NAME,
+                GlobalConstants.CUSTOMER_EMAILID, GlobalConstants.AGENT_NAME );
+            dashboardPage.clickSendSurveyButton();
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testSendSurveyAdmin" );
+            Assert.fail( "Exception Occurred While Testing: testSendSurveyAdmin: " + e.getMessage() );
+        }
+    }
+
+
+    //Test resend survey from dashboard
+    @Test ( groups = "userdashboard", testName = "TCSSUD-46")
+    public void testResendSurveyRequest()
+    {
+        LOG.trace( "\n\n*** Started Testing: testResendSurveyRequest" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickResendSurveyFromDashbaord( 1 );
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testResendSurveyRequest" );
+            Assert.fail( "Exception Occurred While Testing: testResendSurveyRequest: " + e.getMessage() );
+        }
+    }
+
+
+    //Test send survey after login with admin login
+    @Test ( groups = "userdashboard", testName = "TCSSUD-47")
+    public void testRetakeSurveyRequest()
+    {
+        LOG.trace( "\n\n*** Started Testing: testRetakeSurveyRequest" );
+        try {
+            driver.get( GlobalConstants.SERVER_URL );
+            HomePage homePage = new HomePage( driver );
+            DashboardPage dashboardPage = homePage.loginUser( GlobalConstants.COMPANY_USER_NAME,
+                GlobalConstants.COMPANY_USER_PASSWORD );
+            dashboardPage.clickSendSurveyPopupClose();
+            dashboardPage.clickRetakeSurveyFromDashbaord( 1 );
+        } catch ( Exception e ) {
+            e.printStackTrace( System.out );
+            LOG.error( "*** Exception While Testing: testRetakeSurveyRequest" );
+            Assert.fail( "Exception Occurred While Testing: testRetakeSurveyRequest: " + e.getMessage() );
+        }
+    }
 }
