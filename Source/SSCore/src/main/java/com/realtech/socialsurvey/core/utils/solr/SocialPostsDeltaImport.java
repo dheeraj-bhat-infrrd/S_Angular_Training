52a1134440a87eb221fd00a16330ad7157b33c22
ghanashyam <ghanashyamvtatti@gmail.com>
2015-11-24 10:48:21 +0530

    JIRA SS-1287 Fix for last build time issue

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/utils/solr/SocialPostsDeltaImport.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/utils/solr/SocialPostsDeltaImport.java
@@ -12,10 +12,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.realtech.socialsurvey.core.commons.CommonConstants;
 import com.realtech.socialsurvey.core.dao.SocialPostDao;
 import com.realtech.socialsurvey.core.dao.SolrImportDao;
 import com.realtech.socialsurvey.core.entities.SocialPost;
 import com.realtech.socialsurvey.core.exception.NoRecordsFetchedException;
+import com.realtech.socialsurvey.core.services.batchTracker.BatchTrackerService;
 import com.realtech.socialsurvey.core.services.search.SolrSearchService;
 import com.realtech.socialsurvey.core.services.search.exception.SolrException;
 
@@ -38,6 +40,9 @@ public class SocialPostsDeltaImport
     @Autowired
     private SocialPostDao socialPostDao;
 
+    @Autowired
+    private BatchTrackerService batchTrackerService;
+
 
     /**
      * Method to fetch social posts from mongodb and index it into Solr
@@ -49,14 +54,22 @@ public class SocialPostsDeltaImport
         int pageNo = 1;
         List<SocialPost> socialPosts = null;
         Date lastBuildTime = null;
+        boolean errorOccured = false;
+        Long nextLastBuild = 0l;
         if ( fromBeginning ) {
             lastBuildTime = new Date( 0l );
         } else {
             try {
-                Long lastBuild = solrSearchService.getLastBuildTimeForSocialPosts().getTime();
+                //Long lastBuild = solrSearchService.getLastBuildTimeForSocialPosts().getTime();
+                //JIRA SS-1287
+                //Get last build time from batch tracker table
+                Long lastBuild = batchTrackerService
+                    .getLastRunTimeByBatchType( CommonConstants.BATCH_TYPE_SOCIAL_MONITOR_LAST_BUILD );
                 lastBuildTime = new Date( lastBuild );
-            } catch ( SolrException e ) {
-                LOG.error( "SolrException occured while retrieving lastBuildTime via LukeRequestHandler.Reason : ", e );
+                nextLastBuild = System.currentTimeMillis();
+            } catch ( NoRecordsFetchedException e ) {
+                LOG.error( "Unable to fetch lastBuildTime from Batch Tracker table. Reason : ", e );
+                errorOccured = true;
             }
         }
         do {
@@ -79,9 +92,19 @@ public class SocialPostsDeltaImport
                 solrSearchService.addSocialPostsToSolr( socialPosts );
             } catch ( SolrException e ) {
                 LOG.error( "SolrException occurred while adding social posts to solr", e );
+                errorOccured = true;
             }
             pageNo++;
         } while ( !socialPosts.isEmpty() );
+        //Update last build time in batch tracker table
+        if ( !( errorOccured ) ) {
+            try {
+                batchTrackerService.updateModifiedOnColumnByBatchTypeAndTime(
+                    CommonConstants.BATCH_TYPE_SOCIAL_MONITOR_LAST_BUILD, nextLastBuild );
+            } catch ( NoRecordsFetchedException e ) {
+                LOG.error( "Unable to update last build time in batch tracker. Reason : ", e );
+            }
+        }
         LOG.info( "Finished run method of SocialPostsDeltaImport" );
 
     }
