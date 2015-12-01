623a136bb67ec98858da0138367b1384c2c1eeb8
Rijil Krishnan <rijil.krishnan@raremile.com>
2015-12-01 15:43:43 +0530

    SS-1296 RHF issue with Region 1. Fix added to fetch hierarchy data from MySQL 2. Added Test cases

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/BranchDao.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/BranchDao.java
@@ -1,6 +1,9 @@
 package com.realtech.socialsurvey.core.dao;
 
+import java.util.List;
+import java.util.Set;
 import com.realtech.socialsurvey.core.entities.Branch;
+import com.realtech.socialsurvey.core.exception.InvalidInputException;
 
 /*
  * This interface contains methods which are required for queries and criteria on Branch table.
@@ -10,4 +13,5 @@ public interface BranchDao extends GenericDao<Branch, Long> {
 	public void deleteBranchesByCompanyId(long companyId);
 
 
+    public List<Branch> getBranchForBranchIds( Set<Long> branchIds ) throws InvalidInputException;
 }
