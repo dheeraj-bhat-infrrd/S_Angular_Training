3edf79794e8e2abc6a2801caec80b0c5dd6df1eb
Bindu <bindushetty5@gmail.com>
2015-12-30 11:44:08 +0530

    JIRA[SS-1420]Zillow icon in dashboard does not get enabled untill refreshed when new zillow profileName is added.

--- Source/SSWeb/src/main/webapp/resources/js/application.js
+++ Source/SSWeb/src/main/webapp/resources/js/application.js
@@ -9824,7 +9824,8 @@ function saveZillowEmailAddress() {
 	}
 	callAjaxFormSubmit("/zillowSaveInfo.do", function(data) {
 		if(data && data == "success") {
-			loadSocialMediaUrlInSettingsPage();
+			showProfileLinkInEditProfilePage("zillow", "${ profile.socialMediaTokens.zillowToken.zillowScreenName}");
+            loadSocialMediaUrlInSettingsPage();
 			$('#overlay-toast').text("Zillow update successful");
 			showToast();
 		} else {
