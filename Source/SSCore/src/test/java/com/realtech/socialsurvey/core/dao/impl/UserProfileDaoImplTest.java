bcfe80c7f1697a10db1333017e5c14af6a82430a
ghanashyam <ghanashyamvtatti@gmail.com>
2016-01-11 16:46:20 +0530

    Branch move logic update 1. Update regionId in user profiles table 2. Update regions in ss-users core

---
+++ Source/SSCore/src/test/java/com/realtech/socialsurvey/core/dao/impl/UserProfileDaoImplTest.java
@@ -0,0 +1,54 @@
+package com.realtech.socialsurvey.core.dao.impl;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import com.realtech.socialsurvey.core.exception.InvalidInputException;
+
+
+public class UserProfileDaoImplTest
+{
+    @InjectMocks
+    private UserProfileDaoImpl userProfileDaoImpl;
+
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {}
+
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {}
+
+
+    @Before
+    public void setUp() throws Exception
+    {
+        MockitoAnnotations.initMocks( this );
+    }
+
+
+    @After
+    public void tearDown() throws Exception
+    {}
+    
+    
+    @Test ( expected = InvalidInputException.class)
+    public void testUpdateRegionIdForParticularBranchForInvalidRegionId() throws InvalidInputException
+    {
+        userProfileDaoImpl.updateRegionIdForParticularBranch( 0l, 1l );
+    }
+    
+    
+    @Test ( expected = InvalidInputException.class)
+    public void testUpdateRegionIdForParticularBranchForInvalidBranchId() throws InvalidInputException
+    {
+        userProfileDaoImpl.updateRegionIdForParticularBranch( 1l, 0l );
+    }
+}
