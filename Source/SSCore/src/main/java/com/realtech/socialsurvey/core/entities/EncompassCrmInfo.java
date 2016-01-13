1f885ad2a52c58017c3515eb6a7dca7884801674
Bindu <bindushetty5@gmail.com>
2016-01-13 10:14:01 +0530

    Reverted accidental merge

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/EncompassCrmInfo.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/EncompassCrmInfo.java
@@ -1,74 +1,66 @@
 package com.realtech.socialsurvey.core.entities;
 
-public class EncompassCrmInfo extends CRMInfo {
+public class EncompassCrmInfo extends CRMInfo
+{
 
-	private String crm_username;
-	private String crm_password;
-	private String url;
-	private String crm_fieldId;
-	private String state;
-	private int numberOfDays;
-	private String emailAddressForReport;
+    private String crm_username;
+    private String crm_password;
+    private String url;
+    private String crm_fieldId;
 
-	public String getCrm_username() {
-		return crm_username;
-	}
 
-	public void setCrm_username(String crm_username) {
-		this.crm_username = crm_username;
-	}
+    public String getCrm_username()
+    {
+        return crm_username;
+    }
 
-	public String getCrm_password() {
-		return crm_password;
-	}
 
-	public void setCrm_password(String crm_password) {
-		this.crm_password = crm_password;
-	}
+    public void setCrm_username( String crm_username )
+    {
+        this.crm_username = crm_username;
+    }
 
-	public String getUrl() {
-		return url;
-	}
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public String getCrm_password()
+    {
+        return crm_password;
+    }
 
-	public String getCrm_fieldId() {
-		return crm_fieldId;
-	}
 
-	public void setCrm_fieldId(String crm_fieldId) {
-		this.crm_fieldId = crm_fieldId;
-	}
+    public void setCrm_password( String crm_password )
+    {
+        this.crm_password = crm_password;
+    }
 
-	public String getState() {
-		return state;
-	}
 
-	public void setState(String state) {
-		this.state = state;
-	}
+    public String getUrl()
+    {
+        return url;
+    }
 
-	public int getNumberOfDays() {
-		return numberOfDays;
-	}
 
-	public void setNumberOfDays(int numberOfDays) {
-		this.numberOfDays = numberOfDays;
-	}
+    public void setUrl( String url )
+    {
+        this.url = url;
+    }
 
-	public String getEmailAddressForReport() {
-		return emailAddressForReport;
-	}
 
-	public void setEmailAddressForReport(String emailAddressForReport) {
-		this.emailAddressForReport = emailAddressForReport;
-	}
+    public String getCrm_fieldId()
+    {
+        return crm_fieldId;
+    }
 
-	@Override
-	public String toString() {
-		return "crm_username: " + crm_username;
-	}
+
+    public void setCrm_fieldId( String crm_fieldId )
+    {
+        this.crm_fieldId = crm_fieldId;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return "crm_username: " + crm_username;
+    }
 
 }
