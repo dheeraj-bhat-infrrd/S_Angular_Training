01b6119792cfbe3635521811806b1a111f747f9d
Ali Moghadam <alighm@gmail.com>
2016-04-05 13:47:10 -0700

    adding swagger UI for API

--- Source/SSApi/src/main/java/com/realtech/socialsurvey/api/configs/SwaggerConfig.java
+++ Source/SSApi/src/main/java/com/realtech/socialsurvey/api/configs/SwaggerConfig.java
@@ -1,5 +1,43 @@
 package com.realtech.socialsurvey.api.configs;
 
-public class SwaggerConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+
+@EnableWebMvc
+@EnableSwagger
+@Configuration
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
+
+	private SpringSwaggerConfig springSwaggerConfig;
+
+	@Autowired
+	public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+		this.springSwaggerConfig = springSwaggerConfig;
+	}
+
+	@Bean
+	public SwaggerSpringMvcPlugin customImplementation() {
+		return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
+			.apiInfo(apiInfo())
+			.includePatterns(".*.*");
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfo("", "", "", "", "", "");
+		return apiInfo;
+	}
 }
