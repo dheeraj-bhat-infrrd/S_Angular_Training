8e07a1ab559446dac287193778b324404a090900
Ghanashyam <ghanashyamvtatti@gmail.com>
2016-04-06 23:12:02 +0530

    Fix for date not appearing properly in Company Registration Status mails + Hierarchy Upload : Fix for all records showing up in append mode on modification
    * Change extension of company heirarchy report to xlsx
    
    * Fix for date not appearing properly in Company Registration Status mails
    
    * Fix for all records showing up in append mode on modification

--- Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/controller/PaymentController.java
+++ Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/controller/PaymentController.java
@@ -189,13 +189,13 @@ public class PaymentController {
 		     
             //specify details
             Date today = new Date( System.currentTimeMillis() );
-            SimpleDateFormat utcDateFormat = new SimpleDateFormat( "MM/DD/YYYY HH:MM:SS z" );
+            SimpleDateFormat utcDateFormat = new SimpleDateFormat( CommonConstants.DATE_FORMAT_WITH_TZ );
             utcDateFormat.setTimeZone( TimeZone.getTimeZone( "UTC" ) );
 
-            SimpleDateFormat pstDateFormat = new SimpleDateFormat( "MM/DD/YYYY HH:MM:SS z" );
+            SimpleDateFormat pstDateFormat = new SimpleDateFormat( CommonConstants.DATE_FORMAT_WITH_TZ );
             pstDateFormat.setTimeZone( TimeZone.getTimeZone( "PST" ) );
 
-            SimpleDateFormat estDateFormat = new SimpleDateFormat( "MM/DD/YYYY HH:MM:SS z" );
+            SimpleDateFormat estDateFormat = new SimpleDateFormat( CommonConstants.DATE_FORMAT_WITH_TZ );
             estDateFormat.setTimeZone( TimeZone.getTimeZone( "EST" ) );
 
             String details = "First Name : " + user.getFirstName() + "<br/>" + "Last Name : " + user.getLastName() + "<br/>"
