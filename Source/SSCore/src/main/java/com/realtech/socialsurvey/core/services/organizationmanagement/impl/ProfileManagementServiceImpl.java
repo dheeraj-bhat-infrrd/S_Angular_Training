f54cf71bd9d2698a57d8f3e9aa35953b92ea252c
ghanashyam <ghanashyamvtatti@gmail.com>
2016-04-26 17:33:15 +0530

    Image Loader batch refactor

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/organizationmanagement/impl/ProfileManagementServiceImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/organizationmanagement/impl/ProfileManagementServiceImpl.java
@@ -1,6 +1,9 @@
 package com.realtech.socialsurvey.core.services.organizationmanagement.impl;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -19,8 +22,12 @@ import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
 import javax.servlet.UnavailableException;
 
+import com.realtech.socialsurvey.core.services.batchtracker.BatchTrackerService;
+import com.realtech.socialsurvey.core.services.upload.FileUploadService;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.hibernate.HibernateException;
 import org.slf4j.Logger;
@@ -147,6 +154,9 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
     private OrganizationManagementService organizationManagementService;
 
     @Autowired
+    private BatchTrackerService batchTrackerService;
+
+    @Autowired
     private SettingsSetter settingsSetter;
 
     @Autowired
@@ -183,6 +193,9 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
 
     @Autowired
     private UserManagementService userManagementService;
+
+    @Autowired
+    private FileUploadService fileUploadService;
 
     @Autowired
     private SocialManagementService socialManagementService;
@@ -249,6 +262,12 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
 
     @Value ( "${ZILLOW_ENDPOINT}")
     private String zillowEndpoint;
+
+    @Value( "${AMAZON_IMAGE_BUCKET}" )
+    private String amazonImageBucket;
+
+    @Value( "${CDN_PATH}" )
+    private String cdnUrl;
 
     @Autowired
     private ExternalApiCallDetailsDao externalApiCallDetailsDao;
@@ -4856,5 +4875,171 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
                 }
             }
         }
+    }
+
+
+    @Override
+    public void imageLoader() {
+        try {
+            new File( CommonConstants.TEMP_FOLDER ).mkdir();
+
+            // update last start time
+            batchTrackerService.getLastRunEndTimeAndUpdateLastStartTimeByBatchType( CommonConstants.BATCH_TYPE_IMAGE_LOADER,
+                CommonConstants.BATCH_NAME_IMAGE_LOADER );
+
+            // Fetch all the profile images pointing to linkedin for company, regions, branches and individuals.
+            Map<Long, OrganizationUnitSettings> companySettings = organizationManagementService
+                .getSettingsMapWithLinkedinImage( CommonConstants.COMPANY );
+            Map<Long, OrganizationUnitSettings> regionSettings = organizationManagementService
+                .getSettingsMapWithLinkedinImage( CommonConstants.REGION_COLUMN );
+            Map<Long, OrganizationUnitSettings> branchSettings = organizationManagementService
+                .getSettingsMapWithLinkedinImage( CommonConstants.BRANCH_NAME_COLUMN );
+            Map<Long, OrganizationUnitSettings> agentSettings = organizationManagementService
+                .getSettingsMapWithLinkedinImage( "agent" );
+
+            // Process all the company profile images.
+            for ( Map.Entry<Long, OrganizationUnitSettings> companySetting : companySettings.entrySet() ) {
+                try {
+                    String image = loadImages( companySetting.getValue() );
+                    if ( image != null ) {
+                        updateProfileImage(
+                            MongoOrganizationUnitSettingDaoImpl.COMPANY_SETTINGS_COLLECTION, companySetting.getValue(), image );
+                    }
+                } catch ( Exception e ) {
+                    LOG.error( "Exception caught in ImageLoader while copying image from linkedin to SocialSurvey server. "
+                        + "Nested exception is ", e );
+                    continue;
+                }
+            }
+
+            // Process all the region profile images.
+            for ( Map.Entry<Long, OrganizationUnitSettings> regionSetting : regionSettings.entrySet() ) {
+                try {
+                    String image = loadImages( regionSetting.getValue() );
+                    if ( image != null ) {
+                        updateProfileImage(
+                            MongoOrganizationUnitSettingDaoImpl.REGION_SETTINGS_COLLECTION, regionSetting.getValue(), image );
+                    }
+                } catch ( Exception e ) {
+                    LOG.error( "Exception caught in ImageLoader while copying image from linkedin to SocialSurvey server. "
+                        + "Nested exception is ", e );
+                    continue;
+                }
+            }
+
+            // Process all the branch profile images.
+            for ( Map.Entry<Long, OrganizationUnitSettings> branchSetting : branchSettings.entrySet() ) {
+                try {
+                    String image = loadImages( branchSetting.getValue() );
+                    if ( image != null ) {
+                        updateProfileImage(
+                            MongoOrganizationUnitSettingDaoImpl.BRANCH_SETTINGS_COLLECTION, branchSetting.getValue(), image );
+                    }
+                } catch ( Exception e ) {
+                    LOG.error( "Exception caught in ImageLoader while copying image from linkedin to SocialSurvey server. "
+                        + "Nested exception is ", e );
+                    continue;
+                }
+            }
+
+            // Process all the individual profile images.
+            for ( Map.Entry<Long, OrganizationUnitSettings> agentSetting : agentSettings.entrySet() ) {
+                try {
+                    String image = loadImages( agentSetting.getValue() );
+                    if ( image != null ) {
+                        updateProfileImage(
+                            MongoOrganizationUnitSettingDaoImpl.AGENT_SETTINGS_COLLECTION, agentSetting.getValue(), image );
+                    }
+                } catch ( Exception e ) {
+                    LOG.error( "Exception caught in ImageLoader while copying image from linkedin to SocialSurvey server. "
+                        + "Nested exception is ", e );
+                    continue;
+                }
+            }
+
+            //updating last run time for batch in database
+            batchTrackerService.updateLastRunEndTimeByBatchType( CommonConstants.BATCH_TYPE_IMAGE_LOADER );
+            LOG.info( "Completed ImageUploader" );
+        } catch ( Exception e ) {
+            LOG.error( "Error in ImageUploader", e );
+            try {
+                //update batch tracker with error message
+                batchTrackerService.updateErrorForBatchTrackerByBatchType( CommonConstants.BATCH_TYPE_IMAGE_LOADER,
+                    e.getMessage() );
+                //send report bug mail to admin
+                batchTrackerService.sendMailToAdminRegardingBatchError( CommonConstants.BATCH_NAME_IMAGE_LOADER,
+                    System.currentTimeMillis(), e );
+            } catch ( NoRecordsFetchedException | InvalidInputException e1 ) {
+                LOG.error( "Error while updating error message in ImageUploader " );
+            } catch ( UndeliveredEmailException e1 ) {
+                LOG.error( "Error while sending report excption mail to admin " );
+            }
+        }
+    }
+
+    private String loadImages( OrganizationUnitSettings setting ) throws Exception
+    {
+        String linkedinImageUrl = setting.getProfileImageUrl();
+        String imageName = java.util.UUID.randomUUID().toString();
+        if ( linkedinImageUrl.contains( ".png" ) || linkedinImageUrl.contains( ".PNG" ) ) {
+            imageName = imageName + ".png";
+        } else if ( linkedinImageUrl.contains( ".jpg" ) || linkedinImageUrl.contains( ".JPG" ) ) {
+            imageName = imageName + ".jpg";
+        } else if ( linkedinImageUrl.contains( ".jpeg" ) || linkedinImageUrl.contains( ".JPEG" ) ) {
+            imageName = imageName + ".jpeg";
+        }
+
+        String destination = copyImage( linkedinImageUrl, imageName );
+        return destination;
+    }
+
+
+    private BufferedImage getImageFromUrl( String imageUrl )
+    {
+        BufferedImage image = null;
+        try {
+            URL url = new URL( imageUrl );
+            image = ImageIO.read( url );
+        } catch ( IOException e ) {
+            LOG.error( "Exception caught " + e.getMessage() );
+        }
+        return image;
+    }
+
+
+    private String copyImage( String source, String imageName ) throws Exception
+    {
+
+        String fileName = null;
+        try {
+            BufferedImage image = getImageFromUrl( source );
+            if ( image != null ) {
+                File tempImage = new File( CommonConstants.TEMP_FOLDER + CommonConstants.FILE_SEPARATOR + imageName );
+                tempImage.createNewFile();
+                if ( tempImage.exists() ) {
+                    if ( imageName.endsWith( ".jpg" ) || imageName.endsWith( ".JPG" ) ) {
+                        ImageIO.write( image, "jpg", tempImage );
+                    } else if ( imageName.endsWith( ".jpeg" ) || imageName.endsWith( ".JPEG" ) ) {
+                        ImageIO.write( image, "png", tempImage );
+                    } else if ( imageName.endsWith( ".png" ) || imageName.endsWith( ".PNG" ) ) {
+                        ImageIO.write( image, "png", tempImage );
+                    }
+                    fileName = fileUploadService.uploadProfileImageFile( tempImage, imageName, false );
+                    FileUtils
+                        .deleteQuietly( new File( CommonConstants.TEMP_FOLDER + CommonConstants.FILE_SEPARATOR + imageName ) );
+                    LOG.info( "Successfully retrieved photo of contact" );
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } catch ( Exception e ) {
+            LOG.error( e.getMessage() + ": " + source );
+            throw e;
+        }
+
+        return cdnUrl + CommonConstants.FILE_SEPARATOR + amazonImageBucket + CommonConstants.FILE_SEPARATOR + fileName;
+
     }
 }
