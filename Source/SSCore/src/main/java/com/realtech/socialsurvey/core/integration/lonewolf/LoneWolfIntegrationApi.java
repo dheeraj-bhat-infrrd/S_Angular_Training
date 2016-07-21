7faf41a829ab06da9fbc21ee273f8f801c5f71da
Thirumala <thirumala@raremile.com>
2016-07-21 13:05:02 +0530

    Lone Wolf test connection api integration

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/integration/lonewolf/LoneWolfIntegrationApi.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/integration/lonewolf/LoneWolfIntegrationApi.java
@@ -10,5 +10,9 @@ public interface LoneWolfIntegrationApi
     @GET ( "/wolfconnect/transactions/v1?$filter=StatusCode eq 'A'")
     public Response fetchClosedTransactions( @Header ( "Authorization") String authorizationHeader,
         @Header ( "Content-MD5") String md5Content );
+    
+    @GET ( "/wolfconnect/transactions/v1/test")
+    public Response testConnection( @Header ( "Authorization") String authorizationHeader,
+        @Header ( "Content-MD5") String md5Content );
 
 }
