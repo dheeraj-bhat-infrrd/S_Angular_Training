56a890719e91d1fe74cb396fef4e3d686b65a79a
rohitpatidar <rohit.patidar@raremile.com>
2016-08-01 19:06:19 +0530

    Loan wolf entities and enums

---
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/entities/LoanWolfClientContactType.java
@@ -0,0 +1,17 @@
+package com.realtech.socialsurvey.core.entities;
+
+public class LoanWolfClientContactType
+{
+
+    private String Code;
+
+    public String getCode()
+    {
+        return Code;
+    }
+
+    public void setCode( String code )
+    {
+        Code = code;
+    }
+}
