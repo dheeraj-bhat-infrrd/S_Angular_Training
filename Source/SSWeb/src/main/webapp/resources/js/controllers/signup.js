13b453470e9ca49aca9d37a3212b8065afd9f442
rohitpatidar <rohit.patidar@raremile.com>
2016-12-27 11:51:34 +0530

    Added functionality to enable or disable captcha based on a property.

--- Source/SSWeb/src/main/webapp/resources/js/controllers/signup.js
+++ Source/SSWeb/src/main/webapp/resources/js/controllers/signup.js
@@ -120,6 +120,7 @@ app.controller('accountSignupController', [ '$cookies', '$scope', '$location', '
 	$scope.widgetId = null;
 	$scope.isEmailIdExist = false;
     $scope.isNewRecaptcha=false;
+    $scope.enableCaptcha = enableCaptcha;
 	$scope.model = {
 		key : '6Le2wQYTAAAAAAacBUn0Dia5zMMyHfMXhoOh5A7K'
 	};
@@ -136,17 +137,31 @@ app.controller('accountSignupController', [ '$cookies', '$scope', '$location', '
 		$scope.activate = 1;
 	}
 
+	
+	$scope.isCaptchaEnabled=function(){
+		if($scope.enableCaptcha=='Y')
+			return true;
+		else
+			return false;
+	}
+	
 	$scope.submitLogin = function() {
 		if($scope.isEmailIdExist== true){
 			return;
 		}
-		$scope.recaptcha=vcRecaptchaService.getResponse();
-		if($scope.recaptcha==""){
-			return;
+		
+		
+		if($scope.isCaptchaEnabled()){
+			if(vcRecaptchaService.getResponse()==""){
+				return;
+			}
 		}
+		
 		if ($scope.signInForm.$valid) {
 			showOverlay();
-			$scope.accountRegistration.captchaResponse = vcRecaptchaService.getResponse();
+			if($scope.isCaptchaEnabled()){
+				$scope.accountRegistration.captchaResponse = vcRecaptchaService.getResponse();				
+			}
 			$scope.accountRegistration.phone = getPhoneNumber("reg-phone");
 			$scope.accountRegistration.planId = planId;
 			LoginService.signup($scope.accountRegistration).then(function(response) {
