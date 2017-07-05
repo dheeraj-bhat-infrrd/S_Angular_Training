34422a7e296630427329344fcaa6a755a03f9aec
Subhrajit Satpathy <subhrajit@infrrd.ai>
2017-07-05 21:49:15 +0530

    Changed

--- CentralizedDB/DBScripts/6_user_adoption_report.sql
+++ CentralizedDB/DBScripts/6_user_adoption_report.sql
@@ -3,21 +3,16 @@ USE `ss_report_demo`;
 drop table if exists `user_adoption_report`;
 
 CREATE TABLE `user_adoption_report` (
-  `user_adoption_report_id` varchar(36) NOT NULL,
-  `company_id` int(10) unsigned DEFAULT NULL,
-  `company_name` varchar(45) DEFAULT NULL,
-  `region_id` int(10) unsigned DEFAULT NULL,
-  `region_name` varchar(45) DEFAULT NULL,
-  `branch_id` int(10) unsigned DEFAULT NULL,
-  `branch_name` varchar(45) DEFAULT NULL,
+  `user_adoption_report_id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_id` int(10) unsigned NOT NULL,
+  `company_name` varchar(450) DEFAULT NULL,
+  `region_id` int(10) unsigned NOT NULL,
+  `region_name` varchar(450) DEFAULT NULL,
+  `branch_id` int(10) unsigned NOT NULL,
+  `branch_name` varchar(450) DEFAULT NULL,
   `invited_users` int(11) DEFAULT NULL,
   `active_users` int(11) DEFAULT NULL,
   `adoption_rate` decimal(6,2) DEFAULT NULL,
-  PRIMARY KEY (`user_adoption_report_id`),
-  KEY `FK_ADOPTION_COMPANY_idx` (`company_id`),
-  KEY `FK_ADOPTION_REGION_idx` (`region_id`),
-  KEY `FK_ADOPTION_BRANCH_idx` (`branch_id`),
-  CONSTRAINT `FK_ADOPTION_BRANCH` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`BRANCH_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_ADOPTION_COMPANY` FOREIGN KEY (`company_id`) REFERENCES `company` (`COMPANY_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_ADOPTION_REGION` FOREIGN KEY (`region_id`) REFERENCES `region` (`REGION_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`branch_id`,`region_id`,`company_id`),
+  UNIQUE KEY `user_adoption_report_id_UNIQUE` (`user_adoption_report_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
