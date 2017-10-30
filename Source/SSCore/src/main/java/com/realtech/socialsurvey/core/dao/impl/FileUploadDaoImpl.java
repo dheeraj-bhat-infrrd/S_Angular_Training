f49da0d4a8d8270a4fcec4a2b072d6d24036e5da
ssatp812 <subhrajit@infrrd.ai>
2017-10-30 15:45:20 +0530

    Merged complete survey results report branch. (#3705)
    * Survey Results Dropdowns UI
    
    * Survey Results Dropdown changes
    
    * Commit etl changes to zillow count
    
    * API changes for survey results report in company,region,branch,agent heirarchy.
    
    * Fixed join duplicate issue for 3rd party.
    
    * Fixed survey response null.
    
    * Handled null pointer exception for no surveys date range and refactoring.

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/impl/FileUploadDaoImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/dao/impl/FileUploadDaoImpl.java
@@ -34,7 +34,7 @@ public class FileUploadDaoImpl extends GenericDaoImpl<FileUpload, Long> implemen
             criteria.add( Restrictions.eq( CommonConstants.PROFILE_LEVEL_COLUMN , entityType ) );
             criteria.add( Restrictions.eq( CommonConstants.SHOW_ON_UI_COLUMN , true ) );
             criteria.add( Restrictions.in( CommonConstants.FILE_UPLOAD_TYPE_COLUMN, Arrays.asList(
-                CommonConstants.FILE_UPLOAD_REPORTING_SURVEY_STATS_REPORT, CommonConstants.FILE_UPLOAD_REPORTING_VERIFIED_USERS_REPORT , CommonConstants.FILE_UPLOAD_REPORTING_COMPANY_USERS_REPORT, CommonConstants.FILE_UPLOAD_REPORTING_SURVEY_RESULTS_COMPANY_REPORT
+                CommonConstants.FILE_UPLOAD_REPORTING_SURVEY_STATS_REPORT, CommonConstants.FILE_UPLOAD_REPORTING_VERIFIED_USERS_REPORT , CommonConstants.FILE_UPLOAD_REPORTING_COMPANY_USERS_REPORT, CommonConstants.FILE_UPLOAD_REPORTING_SURVEY_RESULTS_REPORT
                 ,CommonConstants.FILE_UPLOAD_REPORTING_SURVEY_TRANSACTION_REPORT,CommonConstants.FILE_UPLOAD_REPORTING_USER_RANKING_MONTHLY_REPORT,CommonConstants.FILE_UPLOAD_REPORTING_USER_RANKING_YEARLY_REPORT) ) );
             if ( startIndex > -1 ) {
                 criteria.setFirstResult( startIndex );
@@ -60,7 +60,7 @@ public class FileUploadDaoImpl extends GenericDaoImpl<FileUpload, Long> implemen
             criteria.add( Restrictions.eq( CommonConstants.PROFILE_LEVEL_COLUMN , entityType ) );
             criteria.add( Restrictions.eq( CommonConstants.SHOW_ON_UI_COLUMN , true ) );
             criteria.add( Restrictions.in( CommonConstants.FILE_UPLOAD_TYPE_COLUMN, Arrays.asList(
-                CommonConstants.FILE_UPLOAD_REPORTING_SURVEY_STATS_REPORT, CommonConstants.FILE_UPLOAD_REPORTING_VERIFIED_USERS_REPORT , CommonConstants.FILE_UPLOAD_REPORTING_COMPANY_USERS_REPORT, CommonConstants.FILE_UPLOAD_REPORTING_SURVEY_RESULTS_COMPANY_REPORT
+                CommonConstants.FILE_UPLOAD_REPORTING_SURVEY_STATS_REPORT, CommonConstants.FILE_UPLOAD_REPORTING_VERIFIED_USERS_REPORT , CommonConstants.FILE_UPLOAD_REPORTING_COMPANY_USERS_REPORT, CommonConstants.FILE_UPLOAD_REPORTING_SURVEY_RESULTS_REPORT
                 ,CommonConstants.FILE_UPLOAD_REPORTING_SURVEY_TRANSACTION_REPORT,CommonConstants.FILE_UPLOAD_REPORTING_USER_RANKING_MONTHLY_REPORT,CommonConstants.FILE_UPLOAD_REPORTING_USER_RANKING_YEARLY_REPORT) ) );
             criteria.setProjection( Projections.rowCount() );
             Long count = (Long) criteria.uniqueResult();
