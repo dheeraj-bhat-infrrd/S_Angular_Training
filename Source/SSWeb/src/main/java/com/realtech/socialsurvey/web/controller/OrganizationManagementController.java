3e9978a4e272a3d0652adfb286e08e15638d13a6
rohitpatidar <rohit.patidar@raremile.com>
2017-11-06 09:33:49 -0800

    Removed unnecessary import

--- Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/controller/OrganizationManagementController.java
+++ Source/SSWeb/src/main/java/com/realtech/socialsurvey/web/controller/OrganizationManagementController.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
