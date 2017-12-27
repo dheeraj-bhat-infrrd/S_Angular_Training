929a5f8f6f22486dae2a66cb3bdfba915efbcd29
YOGANDA-RM\yoganand <yogananda@infrrd.ai>
2017-12-27 18:10:06 +0530

    overview user count etls

---
+++ CentralizedDB/DBScripts/alter_overview_company_region_branch_month.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `ss_centralized_mongodb`.`overview_branch_month` ADD COLUMN `user_count` INT(10) NULL DEFAULT 0 AFTER `year`;
+ALTER TABLE `ss_centralized_mongodb`.`overview_region_month` ADD COLUMN `user_count` INT(10) NULL DEFAULT 0 AFTER `year`;
+ALTER TABLE `ss_centralized_mongodb`.`overview_company_month` ADD COLUMN `user_count` INT(10) NULL DEFAULT 0 AFTER `year`;
