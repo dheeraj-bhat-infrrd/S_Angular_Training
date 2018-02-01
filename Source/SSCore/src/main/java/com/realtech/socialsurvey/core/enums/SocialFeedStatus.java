080e13c624f478c8f088242220ae194a8b14b926
Manish Carpenter <manish.c@infrrd.ai>
2018-02-01 12:18:26 +0530

    Added Social feed type and status enum.

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/enums/SocialFeedStatus.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/enums/SocialFeedStatus.java
@@ -2,5 +2,19 @@ package com.realtech.socialsurvey.core.enums;
 
 public enum SocialFeedStatus
 {
+    NEW( 0 ), ESCALATED( 1 ), RESOLVED( 2 );
+
+    private int value;
+
+    SocialFeedStatus( int value )
+    {
+        this.value = value;
+    }
+
+
+    public int getValue()
+    {
+        return value;
+    }
 
 }
--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/enums/SocialFeedType.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/enums/SocialFeedType.java
@@ -2,5 +2,16 @@ package com.realtech.socialsurvey.core.enums;
 
 public enum SocialFeedType
 {
+    FACEBOOK(0), LINKEDIN(1), TWITTER(2);
+    
+    private int value;
+    
+    SocialFeedType(int value){
+        this.value = value;
+    }
+    
+    public int getValue(){
+        return value;
+    }
 
 }
