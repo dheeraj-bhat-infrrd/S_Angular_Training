1aae20ec8fb9dd707543e6e4d9045904a301705e
manish-infrrd <manish.c@inffrd.ai>
2018-04-02 18:35:34 +0530

    Reduced topology max spout pending to 50.

--- Source/Storm/compute/src/main/java/com/realtech/socialsurvey/compute/SocialPostExtractorTopologyStarterHelper.java
+++ Source/Storm/compute/src/main/java/com/realtech/socialsurvey/compute/SocialPostExtractorTopologyStarterHelper.java
@@ -77,7 +77,7 @@ public class SocialPostExtractorTopologyStarterHelper extends TopologyStarterHel
             return config;
         } else {
             Config config = new Config();
-            config.put( Config.TOPOLOGY_MAX_SPOUT_PENDING, 10 );
+            config.put( Config.TOPOLOGY_MAX_SPOUT_PENDING, 50 );
             config.put( Config.STORM_NIMBUS_RETRY_TIMES, 3 );
             return config;
         }
