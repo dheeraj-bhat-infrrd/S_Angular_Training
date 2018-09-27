4c9c77926a483ee3b698cc73fb805c300f2ab664
DheerajBhat <dheerajbhat@infrrd.ai>
2018-09-27 12:49:14 +0530

    Single review page verified partner fix

--- Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/organizationmanagement/impl/ProfileManagementServiceImpl.java
+++ Source/SSCore/src/main/java/com/realtech/socialsurvey/core/services/organizationmanagement/impl/ProfileManagementServiceImpl.java
@@ -5936,6 +5936,16 @@ public class ProfileManagementServiceImpl implements ProfileManagementService, I
             && StringUtils.isEmpty( review.getSourceId() ) ) {
             review.setSourceId( review.getCompleteProfileUrl() );
         }
+        
+        SurveyPreInitiation surveyPreInitiation = surveyPreInitiationDao.findById(SurveyPreInitiation.class, review.getSurveyPreIntitiationId());
+        if(surveyPreInitiation != null) {
+        	int participantType = surveyPreInitiation.getParticipantType();
+        	if(review.getSource().equals("encompass") || review.getSource().equals("DOTLOOP") || review.getSource().equals("API") || review.getSource().equals("FTP") || review.getSource().equals("LONEWOLF") ) {
+        		if(participantType == 3 || participantType == 4) {
+                	review.setSource("verifiedPartner");
+                }
+        	}                        
+        }
 
         //This is added to get the agent's APP ID and profile URL 
         //DO NOT REMOVE!
