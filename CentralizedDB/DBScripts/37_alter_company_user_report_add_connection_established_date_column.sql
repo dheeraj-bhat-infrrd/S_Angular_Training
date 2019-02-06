f73fcfd3f09df579c6c2a920aacbc7ebd701d717
Yogesh <yogesh@infrrd.ai>
2019-02-06 12:10:11 +0530

    Centralized DB scripts.

--- CentralizedDB/DBScripts/37_alter_company_user_report_add_connection_established_date_column.sql
+++ CentralizedDB/DBScripts/37_alter_company_user_report_add_connection_established_date_column.sql
@@ -1,20 +1,8 @@
 ALTER TABLE `company_user_report` 
-drop COLUMN `fb_data_connection`;
+ADD COLUMN `fb_connection_established_date` datetime;
 
 ALTER TABLE `company_user_report` 
-ADD COLUMN `fb_connection_established_date` datetime AFTER `fb_data_connection`;
+ADD COLUMN `linkedin_connection_established_date` datetime;
 
 ALTER TABLE `company_user_report` 
-drop COLUMN `fb_data_connection`;
-
-ALTER TABLE `company_user_report` 
-ADD COLUMN `linkedin_connection_established_date` datetime AFTER `linkedin_data_connection`;
-
-ALTER TABLE `company_user_report` 
-drop COLUMN `linkedin_data_connection`;
-
-ALTER TABLE `company_user_report` 
-ADD COLUMN `twitter_connection_established_date` datetime AFTER `twitter_data_connection`;
-
-ALTER TABLE `company_user_report` 
-drop COLUMN `twitter_data_connection`;
+ADD COLUMN `twitter_connection_established_date` datetime;
